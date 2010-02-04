f6be8362f1fdbe2ab1cceb790ad8a51bb7b637a7
Joshua A. Anderson <joaander@umich.edu>
2010-02-04 12:11:53 -0500

    Incorporated num_util.
    Set up simple numpy -> c++ test.

---
+++ CMake/freud/FREUDNumpySetup.cmake
@@ -0,0 +1,35 @@
+# sets the variable NUMPY_INCLUDE_DIR for using numpy with python
+find_package(PythonInterp REQUIRED)
+
+if(NOT NUMPY_INCLUDE_DIR)
+
+execute_process(
+    COMMAND
+    ${PYTHON_EXECUTABLE} -c "import numpy; print numpy.get_include()"
+    OUTPUT_VARIABLE NUMPY_INCLUDE_GUESS
+    RESULT_VARIABLE NUMPY_ERR
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+if(NUMPY_ERR)
+    message(STATUS "Error while querying numpy include directory")
+endif(NUMPY_ERR)
+
+# We use the full path name (including numpy on the end), but
+# Double-check that all is well with that choice.
+find_path(
+    NUMPY_INCLUDE_DIR
+    numpy/arrayobject.h
+    HINTS
+    ${NUMPY_INCLUDE_GUESS}
+    )
+
+if (NUMPY_INCLUDE_DIR)
+message(STATUS "Found numpy: ${NUMPY_INCLUDE_DIR}")
+endif (NUMPY_INCLUDE_DIR)
+
+endif(NOT NUMPY_INCLUDE_DIR)
+
+if (NUMPY_INCLUDE_DIR)
+mark_as_advanced(NUMPY_INCLUDE_DIR)
+endif (NUMPY_INCLUDE_DIR)
--- cpp/trajectory.cc
+++ cpp/trajectory.cc
@@ -1,17 +1,41 @@
 #include <iostream>
 #include <boost/python.hpp>
 
+#include "num_util.h"
+
 using namespace std;
 using namespace boost::python;
+namespace bnp=boost::python::numeric;
 
 void hello()
     {
     cout << "hello." << endl;
     }
 
+void test(bnp::array inValue)
+    {
+    num_util::check_type(inValue, PyArray_FLOAT);
+    num_util::check_rank(inValue, 1);
+    float* dataPtr = (float*) num_util::data(inValue);
+    int theSize= num_util::size(inValue);
+    std::cout << std::endl << "data values on c++ side: " << std::endl;
+    for(int i=0;i < theSize;++i)
+        {
+        std::cout << *(dataPtr + i) << std::endl;
+        }
+    }
+ 
+
+
 BOOST_PYTHON_MODULE(_trajectory)
     {
+    // setup needed for numpy
+    import_array();
+    bnp::array::set_module_and_type("numpy", "ndarray");
+
+    // define functions
     def("hello", &hello);
+    def("test", &test);
     }
 
 
