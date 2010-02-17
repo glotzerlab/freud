2c1eace4e9f15f2fc226e7924be528b1f410da99
Joshua A. Anderson <joaander@umich.edu>
2010-02-17 11:12:30 -0500

    Importing LinkCell into python package hierarchy

---
+++ py/freud/locality.py
@@ -0,0 +1,17 @@
+## \package freud.trajectory
+#
+# Computes locality information on sets of points
+#
+# The following classes are imported into locality from C++:
+#  * LinkCell
+#  * IteratorLinkCell
+
+# bring related c++ classes into the locality module
+from _freud import LinkCell
+from _freud import IteratorLinkCell
+
+
+# fixup IteratorLinkCell with an __iter__ method
+def iterator_link_cell_iter(self):
+    return self
+IteratorLinkCell.__iter__ = iterator_link_cell_iter;
