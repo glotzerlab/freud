80a0f6b843d865766d30bd52adab12cf1b5a37fc
Joshua A. Anderson <joaander@umich.edu>
2011-01-26 15:27:39 -0500

    Updated VMDTrajectory to work with the new position data format.
    Also fleshed out the VMDTrajectory capability, it now reports everything that it can about the system and allows writeback of the user flags (allowing writeback of position,type, etc.. just doesn't make sense for an analysis code)

--- py/freud/trajectory.py
+++ py/freud/trajectory.py
@@ -17,6 +17,16 @@ from _freud import Box;
 # The following classes are imported into trajectory from c++:
 #  * Box
 
+## \internal
+# \brief Takes in a list of particle types and uniquely determines type ids for each one
+# \note While typids will remain the same when run with the same types in the box, regardless of order, if one
+# or another types are missing, then the typid assigments will differ. Thus, this is not to be used for trajectories
+# where types vary from frame to frame and may dissapear
+def _assign_typeid(typename):
+    l = list(set(typename));
+    l.sort();
+    return [l.index(t) for t in typename];
+
 class TrajectoryIter:
     def __init__(self, traj):
         self.traj = traj;
@@ -59,7 +69,19 @@ class TrajectoryVMD:
         # save the static properties
         self.static_props = {};
         self.static_props['mass'] = numpy.array(self.all.get('mass'), dtype='float32');
-        self.static_props['diameter'] = 2.0*numpy.array(self.all.get('radius'), dtype='float32');        
+        self.static_props['diameter'] = 2.0*numpy.array(self.all.get('radius'), dtype='float32');
+        self.static_props['typename'] = self.all.get('type');
+        self.static_props['typeid'] = _assign_typeid(self.static_props['typename']);
+        self.static_props['body'] = numpy.array(self.all.get('resid'), dtype=numpy.int32);
+        self.static_props['charge'] = numpy.array(self.all.get('charge'), dtype=numpy.float32);
+        
+        self.modifiable_props = ['user', 'user2', 'user3', 'user4'];
+        
+    ## Test if a given particle property is modifiable
+    # \param prop Property to check
+    # \returns True if \a prop is modifiable
+    def isModifiable(self, prop):
+        return prop in self.modifiable_props;
 
     ## Test if a given particle property is static over the length of the trajectory
     # \param prop Property to check
@@ -82,30 +104,56 @@ class TrajectoryVMD:
     def __len__(self):
         return self.mol.numFrames();
     
+    ## Sets the current frame
+    # \param idx Index of the frame to seek to
+    def setFrame(self, idx):
+        self.mol.setFrame(idx);
+    
     ## Get the current frame
     # \returns A FrameVMD containing the current frame data
     def getCurrentFrame(self):
         dynamic_props = {};
-        for prop in ['x', 'y', 'z']:
-            dynamic_props[prop] = numpy.array(self.all.get(prop), dtype='float32');
+
+        # get position
+        pos = numpy.zeros(shape=(self.numParticles(),3), dtype=numpy.float32);
+        pos[:,0] = numpy.array(self.all.get('x'));
+        pos[:,1] = numpy.array(self.all.get('y'));
+        pos[:,2] = numpy.array(self.all.get('z'));
+        dynamic_props['position'] = pos;
+        
+        # get user flags
+        for prop in ['user', 'user2', 'user3', 'user4']:
+            dynamic_props[prop] = numpy.array(self.all.get(prop), dtype=numpy.float32)
         
         vmdbox = VMD.molecule.get_periodic(self.mol_id, self.mol.curFrame())
         box = Box(vmdbox['a'], vmdbox['b'], vmdbox['c']);
         return Frame(self, self.mol.curFrame(), dynamic_props, box);
     
-    ## Get the current frame
+    ## Get the selected frame
     # \param idx Index of the frame to access
     # \returns A Frame containing the current frame data
     def __getitem__(self, idx):
         if idx < 0 or idx >= len(self):
             raise IndexError('Frame index out of range');
         
-        self.mol.setFrame(idx);
+        self.setFrame(idx);
         return self.getCurrentFrame();
     
     ## Iterate through frames
     def __iter__(self):
         return TrajectoryIter(self);
+    
+    ## Modify properties of the currently set frame
+    # \param prop Name of property to modify
+    # \param value New values to set for that property
+    def setProperty(self, prop, value):
+        # error check
+        if not prop in self.modifiable_props:
+            raise ValueError('prop is not modifiable');
+        if len(value) != self.numParticles():
+            raise ValueError('value is not of the correct length');
+        
+        self.all.set(prop, list(value));
 
 ## Frame information represneting the system state at a specific frame in a Trajectory
 #
@@ -133,10 +181,8 @@ class Frame:
     #
     # Particle properties are returned as numpy arrays with one element per particle. Properties are queried by name.
     # Common properties are
-    #  - 'x' - x coordiantes
-    #  - 'y' - y coordiantes
-    #  - 'z' - z coordiantes
-    #  - 'mass' - particle masses
+    #  - 'position' - particle positions (Nx3 array)
+    #  - 'mass' - particle masses (Nx1 array)
     #
     # Some types of Trajectories may provide other properties. See their documentation for details.
     def get(self, prop):
@@ -147,4 +193,15 @@ class Frame:
         else:
             raise KeyError('Particle property ' + prop + ' not found');
     
+    ## Set properties for this frame
+    # \param prop Name of property to modify
+    # \param value New values to set for that property 
+    #
+    # Some types of properties can be set, depending on the Trajectory. For example, TrajectoryVMD allows setting of
+    # the user, user2, user3, and user4 flags. Check if a property is modifiable with Trajectory.isModifiable()
+    #
+    # Some types of Trajectories may provide other modifiable properties. See their documentation for details.
+    def set(self, prop, value):
+        self.traj.setFrame(self.frame);
+        self.traj.setProperty(prop, value);
     
