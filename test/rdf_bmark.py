08c26610dde10ad5b461143578a46e0e1b5f1447
Antonio Osorio <aosorio@umich.edu>
2012-10-28 17:55:57 -0400

    Fixed rdf_bmark, point array declaration was incorrect

--- test/rdf_bmark.py
+++ test/rdf_bmark.py
@@ -7,16 +7,14 @@ import time
 N = 64000;
 L = 55.0;
 
-x = numpy.zeros(N, dtype='float32')
-y = numpy.zeros(N, dtype='float32')
-z = numpy.zeros(N, dtype='float32')
+points = numpy.zeros(shape=(N,3), dtype=numpy.float32)
 
 box = trajectory.Box(L);
 
 for i in xrange(0,N):
-    x[i] = (random.random() - 0.5) * L
-    y[i] = (random.random() - 0.5) * L
-    z[i] = (random.random() - 0.5) * L
+    points[i,0] = (random.random() - 0.5) * L
+    points[i,1] = (random.random() - 0.5) * L
+    points[i,2] = (random.random() - 0.5) * L
 
 # benchmark rdf
 trials = 5;
@@ -24,10 +22,10 @@ avg_time = 0;
 
 # warm up
 rdf = density.RDF(box, 5.0, 0.05)
-rdf.compute(x,y,z, x,y,z);
+rdf.compute(points, points);
 
 start = time.time();
 for trial in xrange(0,trials):
-    rdf.compute(x,y,z, x,y,z);
+    rdf.compute(points, points);
 end = time.time();
 print 'avg time per trial:', (end-start)/float(trials)
