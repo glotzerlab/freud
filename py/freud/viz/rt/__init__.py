2fa499e7ce66238bd20618502b6c043e4d77caf5
Matthew Spellings <mspells@umich.edu>
2013-06-17 14:00:07 -0400

    Fixed a bug in saving and loading settings

--- py/freud/viz/rt/__init__.py
+++ py/freud/viz/rt/__init__.py
@@ -33,7 +33,7 @@ from . import rastergl
 
 ## \package freud.viz.rt
 #
-# Real-time visualization Qt widgets and rendering routines. freud.qt.init_app() must be called prior to constructing 
+# Real-time visualization Qt widgets and rendering routines. freud.qt.init_app() must be called prior to constructing
 # any class in rt.
 #
 # \note freud.viz.rt **requires** pyside and pyopengl. If these dependencies are not present, a warning is issued to the
@@ -46,7 +46,7 @@ null = c_void_p(0)
 #
 # GLWidget renders a Scene in real-time using OpenGL. It is a low-level widget that can be embedded in other windows.
 # MainWindow embeds a central GLWidget around a feature-providing interface.
-# 
+#
 # It (currently) only offers 2D camera control. Updates to the
 # camera are made directly in the reference scene, so code external to GLWidget that uses the same scene will render
 # the same point of view.
@@ -68,21 +68,21 @@ class GLWidget(QtOpenGL.QGLWidget):
     # animation states the UI code can take
     ANIM_IDLE = 1;
     ANIM_PAN = 2;
-    
+
     ## Create a GLWidget
     # \param scene the Scene to render
-    # \param *args non-keyword args passed on to QGLWidget 
-    # \param **kwargs keyword args passed on to QGLWidget 
+    # \param *args non-keyword args passed on to QGLWidget
+    # \param **kwargs keyword args passed on to QGLWidget
     #
     def __init__(self, scene, *args, **kwargs):
         if not qt.is_initialized():
             raise RuntimeError('freud.qt.init_app() must be called before constructing a GLWidget');
-        
+
         QtOpenGL.QGLWidget.__init__(self, *args, **kwargs)
         self.scene = scene;
-        
+
         self.setCursor(QtCore.Qt.OpenHandCursor)
-        
+
         # initialize state machine variables
         self._anim_state = GLWidget.ANIM_IDLE;
         self._prev_pos = numpy.array([0,0], dtype=numpy.float32);
@@ -90,27 +90,27 @@ class GLWidget(QtOpenGL.QGLWidget):
         self._pan_vel = numpy.array([0,0], dtype=numpy.float32);
         self._initial_pan_vel = numpy.array([0,0], dtype=numpy.float32);
         self._initial_pan_time = time.time();
-        
+
         # timer for the animation loop
         self._timer_animate = QtCore.QTimer(self)
         self._timer_animate.timeout.connect(self.animate)
         self._timer_animate.start();
-        
+
         self.setFocusPolicy(QtCore.Qt.ClickFocus);
-    
+
     ## Set a new scene
     #
     def setScene(self, scene):
         self.scene = scene;
         self._anim_state == GLWidget.ANIM_IDLE
-    
+
         if self.scene is not None:
             w = self.width();
             h = self.height();
             self.scene.camera.setAspect(w/h);
             self.scene.camera.resolution = h;
-    
-    ## \internal 
+
+    ## \internal
     # \brief Resize the GL viewport
     #
     # Set the gl viewport size and update the camera resolution
@@ -120,7 +120,7 @@ class GLWidget(QtOpenGL.QGLWidget):
         if self.scene is not None:
             self.scene.camera.setAspect(w/h);
             self.scene.camera.resolution = h;
-    
+
     ## \internal
     # \brief Paint the GL scene
     #
@@ -159,36 +159,36 @@ class GLWidget(QtOpenGL.QGLWidget):
         # If we are idle, stop the timer
         if self._anim_state == GLWidget.ANIM_IDLE or self.scene is None:
             self._timer_animate.stop();
-        
+
         # If we are panning
         elif self._anim_state == GLWidget.ANIM_PAN:
             # Decrease the pan velocity on an exponential curve
             cur_time = time.time();
             self._pan_vel = numpy.exp(-(cur_time - self._initial_pan_time)/0.1) * self._initial_pan_vel;
-            
+
             # Compute a delta (in camera units) and move the camera
             delta = self._pan_vel * (cur_time - self._prev_time) * self.scene.camera.pixel_size;
             delta[0] = delta[0] * -1;
             self.scene.camera.position[0:2] += delta;
             self._prev_time = cur_time;
-            
+
             # Go back to the idle state when we come to a stop
             if numpy.dot(self._pan_vel, self._pan_vel) < 100:
                 self._anim_state = GLWidget.ANIM_IDLE;
-            
+
             # Redraw the GL view
             self.updateGL();
 
     ## \internal
     # \brief Close event
     #
-    # Releases OpenGL resources when the widget is closed. 
+    # Releases OpenGL resources when the widget is closed.
     #
     def closeEvent(self, event):
         # stop the animation loop
         self._anim_state = GLWidget.ANIM_IDLE;
         self._timer_animate.stop();
-        
+
         # make the gl context current and free resources
         self.makeCurrent();
         self.draw_gl.destroy();
@@ -211,16 +211,16 @@ class GLWidget(QtOpenGL.QGLWidget):
             delta = (cur_pos - self._prev_pos) * self.scene.camera.pixel_size;
             delta[0] = delta[0] * -1;
             self.scene.camera.position[0:2] += delta;
-            
+
             # compute pan velocity in camera pixels/second
             self._pan_vel[:] = (cur_pos - self._prev_pos) / (cur_time - self._prev_time);
-            
+
             self._prev_time = cur_time;
             self._prev_pos = cur_pos;
-            
+
             # Redraw the GL view
             self.updateGL();
-            
+
             event.accept();
         else:
             event.ignore();
@@ -234,11 +234,11 @@ class GLWidget(QtOpenGL.QGLWidget):
         if self.scene is None:
             event.ignore();
             return;
-        
+
         if event.button() == QtCore.Qt.LeftButton:
             # stop any running pan animations
             self._anim_state = GLWidget.ANIM_IDLE;
-            
+
             # save the drag start position, time, and update the cursor
             self._prev_pos[0] = event.x();
             self._prev_pos[1] = event.y();
@@ -259,7 +259,7 @@ class GLWidget(QtOpenGL.QGLWidget):
         if self.scene is None:
             event.ignore();
             return;
-        
+
         if event.button() == QtCore.Qt.LeftButton:
             # switch to the panning animation, set the initial pan vel and time, and update the cursor
             self._anim_state = GLWidget.ANIM_PAN;
@@ -270,7 +270,7 @@ class GLWidget(QtOpenGL.QGLWidget):
             event.accept();
         else:
             event.ignore();
-    
+
     ## \internal
     # \brief Zoom in response to a mouse wheel event
     #
@@ -278,18 +278,18 @@ class GLWidget(QtOpenGL.QGLWidget):
         if self.scene is None:
             event.ignore();
             return;
-        
+
         # control speed based on modifiers (ctrl/cmd = slow)
         if event.modifiers() == QtCore.Qt.ControlModifier:
             speed = 0.05;
         else:
             speed = 0.2;
-        
+
         if event.orientation() == QtCore.Qt.Vertical:
             # zoom the camera based on the mouse wheel. Zooming is a constant factor reduction (or increase) in size.
             f = 1 - speed * float(event.delta())/120;
             self.scene.camera.setHeight(self.scene.camera.getHeight() * f);
-            
+
             # Redraw the GL view
             self.updateGL();
             event.accept();
@@ -315,14 +315,14 @@ class GLWidget(QtOpenGL.QGLWidget):
 #
 class SceneUpdateManager(QtCore.QObject):
     completed = QtCore.Signal(int);
-    
+
     def __init__(self, scene):
         QtCore.QObject.__init__(self);
         self.scene = scene;
 
     @QtCore.Slot()
     def initialize(self):
-        self._target_frame = None;        
+        self._target_frame = None;
         self._timer = QtCore.QTimer(self)
         self._timer.timeout.connect(self.process)
 
@@ -334,7 +334,7 @@ class SceneUpdateManager(QtCore.QObject):
             self.scene.setFrame(self._target_frame);
             self.completed.emit(self._target_frame);
             self._target_frame = None;
-    
+
     @QtCore.Slot()
     def loadFrame(self, target_frame):
         self._target_frame = target_frame;
@@ -347,21 +347,21 @@ class SceneUpdateManager(QtCore.QObject):
 class TrajectoryViewer(QtGui.QMainWindow):
     frame_select = QtCore.Signal(int);
     frame_display = QtCore.Signal(int);
-    
+
     def __init__(self, scene=None, immediate=False, dock_widgets=[], toolbars=[], *args, **kwargs):
         QtGui.QMainWindow.__init__(self, *args, **kwargs)
 
         self.scene = scene;
         self._frame = -1;
         self._immediate = immediate;
-        
+
         self.setWindowTitle('freud.viz')
 
         self.timer_animate = QtCore.QTimer(self)
         self.timer_animate.timeout.connect(self.gotoNextFrame)
 
         self.statusBar().showMessage('Ready');
-        
+
         # initialize the scene update manager
         if not self._immediate:
             self.update_manager = SceneUpdateManager(scene);
@@ -375,30 +375,30 @@ class TrajectoryViewer(QtGui.QMainWindow):
         for toolbar in toolbars:
             toolbar.setParent(self);
             self.addToolBar(toolbar);
-        
+
         self.createActions();
         self.createToolbars();
-        
+
         for widget in dock_widgets:
             self.addDockWidget(QtCore.Qt.RightDockWidgetArea, widget);
-            
+
         self.createMenus();
         self.restoreSettings();
-        
+
         # initialize the gl display
         self.glWidget = GLWidget(scene)
         self.setCentralWidget(self.glWidget)
-        
+
         self.gotoFrame(0);
 
-    
+
     ## Create the actions
     def createActions(self):
         self.action_close = QtGui.QAction('&Close', self);
         self.action_close.setShortcut('Ctrl+W');
         self.action_close.setStatusTip('Close window');
         self.action_close.triggered.connect(self.close);
-    
+
         self.action_play = QtGui.QAction('&Play', self);
         self.action_play.setShortcut('Space');
         self.action_play.setStatusTip('Play or pause the animation');
@@ -414,7 +414,7 @@ class TrajectoryViewer(QtGui.QMainWindow):
         self.action_prev.setShortcut('Left');
         self.action_prev.setStatusTip('Go to the previous animation frame');
         self.action_prev.triggered.connect(self.gotoPrevFrame);
-        
+
         self.action_first = QtGui.QAction('&First frame', self);
         self.action_first.setShortcut('Home');
         self.action_first.setStatusTip('Go to the first animation frame');
@@ -429,7 +429,7 @@ class TrajectoryViewer(QtGui.QMainWindow):
     def createMenus(self):
         viz_menu = self.menuBar().addMenu('&Viz')
         viz_menu.addAction(self.action_close);
-        
+
         animate_menu = self.menuBar().addMenu('&Animate');
         animate_menu.addAction(self.action_play);
         animate_menu.addAction(self.action_prev);
@@ -441,24 +441,24 @@ class TrajectoryViewer(QtGui.QMainWindow):
         popup.setTitle('Window');
         window_menu = self.menuBar().addMenu(popup);
 
-        
+
     ## Create sub widgets
     def createSubWidgets(self):
         #self.anim_control = AnimationControl();
-        
+
         #self.anim_control_dock = QtGui.QDockWidget("Animation", self)
         #self.anim_control_dock.setAllowedAreas(QtCore.Qt.TopDockWidgetArea | QtCore.Qt.BottomDockWidgetArea)
         #self.anim_control_dock.setWidget(self.anim_control)
         #self.addDockWidget(QtCore.Qt.BottomDockWidgetArea, self.anim_control_dock)
         pass;
-    
+
     ## Create tool bars
     def createToolbars(self):
         if self.scene is None:
             n_frames = 1;
         else:
             n_frames = self.scene.getNumFrames();
-        
+
         # initialize the non tool button interface elements
         self.frame_slider = QtGui.QSlider(QtCore.Qt.Horizontal, self);
         self.frame_slider.valueChanged[int].connect(self.gotoFrame);
@@ -467,7 +467,7 @@ class TrajectoryViewer(QtGui.QMainWindow):
         self.frame_slider.setStatusTip('Select frame');
         self.frame_slider.setFocusPolicy(QtCore.Qt.NoFocus);
         self.frame_slider.setMaximum(n_frames-1);
-        
+
         self.frame_spinbox = QtGui.QSpinBox(self);
         self.frame_spinbox.setKeyboardTracking(False);
         self.frame_spinbox.setStatusTip('Select frame');
@@ -475,12 +475,12 @@ class TrajectoryViewer(QtGui.QMainWindow):
         self.frame_spinbox.setWrapping(True);
         self.frame_spinbox.setSuffix(' / '+str(n_frames));
         self.frame_spinbox.setMaximum(n_frames-1);
-        
+
         self.fps_spinbox = QtGui.QSpinBox(self);
         self.fps_spinbox.setRange(0,60);
         self.fps_spinbox.setStatusTip('Set maximum animation FPS (0 => unlimited)');
         self.fps_spinbox.valueChanged[int].connect(self.setFPS)
-        
+
         self.animation_control_toolbar = QtGui.QToolBar('Animation', self);
         self.animation_control_toolbar.setObjectName('animation_control_toolbar');
         self.animation_control_toolbar.addAction(self.action_play);
@@ -490,20 +490,20 @@ class TrajectoryViewer(QtGui.QMainWindow):
         self.animation_control_toolbar.addWidget(QtGui.QLabel(text='FPS:', parent=self));
         self.animation_control_toolbar.addWidget(self.fps_spinbox);
         self.animation_control_toolbar.setAllowedAreas(QtCore.Qt.TopToolBarArea | QtCore.Qt.BottomToolBarArea);
-        
+
         self.addToolBar(self.animation_control_toolbar);
-    
+
     ## restore saved settings
     def restoreSettings(self):
         settings = QtCore.QSettings("umich.edu", "freud.viz");
         fps = settings.value('rt-TrajectoryViewer/fps');
         if fps is not None:
             self.setFPS(fps);
-            
+
         geom = settings.value("rt-TrajectoryViewer/geometry");
         if geom is not None:
             self.restoreGeometry(geom);
-        
+
         state = settings.value("rt-TrajectoryViewer/window_state");
         if state is not None:
             self.restoreState(state);
@@ -513,15 +513,15 @@ class TrajectoryViewer(QtGui.QMainWindow):
         self.timer_animate.stop();
         if not self._immediate:
             self.update_thread.quit();
-        
+
         settings = QtCore.QSettings("umich.edu", "freud.viz");
         settings.setValue("rt-TrajectoryViewer/geometry", self.saveGeometry());
-        settings.setValue("rt-TrajectoryViewer/fps", self.fps_spinbox.value());
+        settings.setValue("rt-TrajectoryViewer/fps", int(self.fps_spinbox.value()));
         settings.setValue("rt-TrajectoryViewer/window_state", self.saveState());
         self.centralWidget().closeEvent(event);
         QtGui.QMainWindow.closeEvent(self, event);
 
-    
+
     ## Set the scene
     def setScene(self, scene):
         self.scene = scene;
@@ -538,17 +538,17 @@ class TrajectoryViewer(QtGui.QMainWindow):
         self.frame_spinbox.setSuffix(' / '+str(n_frames));
         self.frame_spinbox.setMaximum(n_frames-1);
         self.glWidget.setScene(scene);
-        
+
         self.gotoFrame(0);
-    
+
     ## Forcibly reload the current animation frame
     def reloadFrame(self):
         frame = self._frame;
         if self._frame is not None:
             self._frame = None;
             self.gotoFrame(frame);
-    
-        
+
+
     ## Set the animation frame
     @QtCore.Slot(int)
     def gotoFrame(self, frame):
@@ -557,30 +557,30 @@ class TrajectoryViewer(QtGui.QMainWindow):
         if frame == self._frame:
             return;
         self._frame = frame;
-        
+
         # sync the two separate controls together
         self.frame_slider.setValue(frame);
         self.frame_spinbox.setValue(frame);
-        
+
         if self.scene is not None:
             # update to the target frame
             if self._immediate:
                 self.scene.setFrame(frame);
                 self.frame_display.emit(frame);
                 self.glWidget.updateGL();
-                            
+
             self.frame_select.emit(frame);
-    
+
     ## Set the maximum FPS
     @QtCore.Slot(int)
     def setFPS(self, fps):
         self.fps_spinbox.setValue(fps);
-        
+
         if fps == 0:
             self.timer_animate.setInterval(0);
         else:
             self.timer_animate.setInterval(1000/fps);
-    
+
     ## Play/pause the animation
     @QtCore.Slot()
     def play(self, play=True):
@@ -588,34 +588,34 @@ class TrajectoryViewer(QtGui.QMainWindow):
             self.timer_animate.start();
         else:
             self.timer_animate.stop();
-    
+
     ## Advance to the next frame
     @QtCore.Slot()
     def gotoNextFrame(self):
         self.frame_spinbox.stepUp();
-    
+
     ## Go back one frame
     @QtCore.Slot()
     def gotoPrevFrame(self):
         self.frame_spinbox.stepDown();
-    
+
     ## Go to start frame
     @QtCore.Slot()
     def gotoFirstFrame(self):
         self.frame_slider.setValue(0);
-    
+
     ## Go to last frame
     @QtCore.Slot()
     def gotoLastFrame(self):
         self.frame_slider.setValue(self.frame_slider.maximum());
-    
+
     ## Present a new frame
     @QtCore.Slot()
     def presentFrame(self, frame):
         self.update();
         self.frame_display.emit(frame);
-        
-    
+
+
 ##########################################
 # Module init
 
