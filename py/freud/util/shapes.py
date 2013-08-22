d95a13c079b9ef4c5caeb4aede21cdff19850e97
Matthew Spellings <mspells@umich.edu>
2013-08-22 13:24:31 -0400

    Added some pieces to the Polygon class

--- py/freud/util/shapes.py
+++ py/freud/util/shapes.py
@@ -1,3 +1,5 @@
+from collections import deque
+from itertools import islice
 import numpy
 from freud.util import trimath
 
@@ -42,52 +44,150 @@ class outline:
         if not sign_sum < 0.0:
             raise TypeError("vertices must be listed in counter-clockwise order")
 
-class polygon:
-    # vertices must be in counter-clockwise order
+def twiceTriangleArea(p0, p1, p2):
+    """Returns twice the signed area of the triangle specified by the
+    2D numpy points (p0, p1, p2)."""
+    p1 = p1 - p0
+    p2 = p2 - p0
+    return p1[0]*p2[1] - p2[0]*p1[1]
+
+def linesCross(line1, line2, tol=1e-5):
+    """Returns true if two lines (specified as pairs of 2D numpy
+    points) cross. Also takes a tolerance cutoff for [twice] the area
+    between three colinear points"""
+    # sign of triangle (line1[0], line2[0], line2[1])
+    area1 = twiceTriangleArea(line1[0], line2[0], line2[1])
+    sign1 = numpy.abs(area1) > tol and int(numpy.sign(area1))
+
+    # sign of triangle (line1[1], line2[0], line2[1])
+    area2 = twiceTriangleArea(line1[1], line2[0], line2[1])
+    sign2 = numpy.abs(area2) > tol and int(numpy.sign(area2))
+
+    # sign of triangle (line2[0], line1[0], line1[1])
+    area3 = twiceTriangleArea(line2[0], line1[0], line1[1])
+    sign3 = numpy.abs(area3) > tol and int(numpy.sign(area3))
+
+    # sign of triangle (line2[1], line1[0], line1[1])
+    area4 = twiceTriangleArea(line2[1], line1[0], line1[1])
+    sign4 = numpy.abs(area4) > tol and int(numpy.sign(area4))
+
+    # specify that if two lines share a vertex (one of the areas is
+    # 0), they don't cross
+    return all([sign1, sign2, sign3, sign4, (sign1 != sign2), (sign3 != sign4)])
+
+def linesTouch(line1, line2):
+    """Returns true if two lines (specified as pairs of 2D numpy
+    points) touch, including sharing a vertex at two endpoints. Also
+    takes a tolerance cutoff for [twice] the area between three
+    colinear points"""
+    # sign of triangle (line1[0], line2[0], line2[1])
+    area1 = twiceTriangleArea(line1[0], line2[0], line2[1])
+    sign1 = numpy.sign(area1)
+
+    # sign of triangle (line1[1], line2[0], line2[1])
+    area2 = twiceTriangleArea(line1[1], line2[0], line2[1])
+    sign2 = numpy.sign(area2)
+
+    # sign of triangle (line2[0], line1[0], line1[1])
+    area3 = twiceTriangleArea(line2[0], line1[0], line1[1])
+    sign3 = numpy.sign(area3)
+
+    # sign of triangle (line2[1], line1[0], line1[1])
+    area4 = twiceTriangleArea(line2[1], line1[0], line1[1])
+    sign4 = numpy.sign(area4)
+
+    return (sign1 != sign2) and (sign3 != sign4)
+
+class Polygon:
+    """Basic class to hold a set of points for a 2D polygon"""
     def __init__(self, verts):
+        """Initialize a polygon with a counterclockwise list of 2D
+        points and checks that they are ordered counter-clockwise"""
         self.vertices = numpy.array(verts, dtype=numpy.float32);
+
         if len(self.vertices) < 3:
             raise TypeError("a polygon must have at least 3 vertices")
         if len(self.vertices[1]) != 2:
             raise TypeError("positions must be an Nx2 array")
         self.n = len(self.vertices)
-        self.is_cclockwise()
 
-    def is_cclockwise(self):
-        sign_sum = 0.0
-        for j in range(self.n):
-            # set up the two sets of verts
-            if j == 0:
-                i = self.n - 1
-            else:
-                i = j - 1
-            if j == (self.n - 1):
-                k = 0
-            else:
-                k = j + 1
-            # math from stackoverflow
-            # http://stackoverflow.com/questions/1165647/how-to-determine-if-a-list-of-polygon-points-are-in-clockwise-order
-            ij = self.vertices[i] - self.vertices[j]
-            jk = self.vertices[k] - self.vertices[j]
-            edge_ij = numpy.array([ij[0], ij[1], 0], dtype=numpy.float32)
-            edge_jk = numpy.array([jk[0], jk[1], 0], dtype=numpy.float32)
-            tmp_sign = trimath.sinecheck(edge_ij, edge_jk)
-            sign_sum += tmp_sign
-        if not sign_sum < 0.0:
-            raise TypeError("vertices must be listed in counter-clockwise order")
+        # This actually checks that the majority of the polygon is
+        # listed in counter-clockwise order, but seems like it should
+        # be sufficient for common use cases. Non-simple polygons can
+        # still sneak in clockwise vertices.
+        if self.area() < 0:
+            raise RuntimeError("Polygon was given with some clockwise vertices, "
+                               "but it requires that vertices be listed in "
+                               "counter-clockwise order")
 
-    # This method is primarily used by the triangulation class in triangulate.py
-    def remove_vert(self, i):
-        #print("vertex removed:")
-        #print(self.vertices[i])
-        self.vertices = numpy.delete(self.vertices, (i), axis=0)
-        self.n = len(self.vertices)
+    def area(self):
+        """Calculate the signed area of the polygon with
+        counterclockwise shapes having positive area"""
+        shifted = numpy.roll(self.vertices, -1, axis=0)
+
+        # areas is twice the signed area of each triangle in the shape
+        areas = self.vertices[:, 0]*shifted[:, 1] - shifted[:, 0]*self.vertices[:, 1]
+
+        return numpy.sum(areas)/2
+
+    def center(self):
+        """Center this polygon around (0, 0)"""
+        self.vertices -= numpy.mean(self.vertices, axis=0)
 
     def print_verts(self):
-        for i in range(self.n):
-            v = self.vertices[i]
-            print("vertex {0}".format(i))
-            print(v)
+        for vert in self.vertices:
+            print("vertex {}".format(vert))
+
+    def splitMonotone(self):
+        """Returns a list of subpolygons of this polygon where each
+        subpolygon is guaranteed to be monotone with respect to any
+        vertical line."""
+        sortIdx = numpy.argsort(self.vertices[:, 0], kind='mergesort')
+        print(numpy.diff(sortIdx))
+        print(numpy.abs(numpy.diff(sortIdx)) == 1)
+        print(self.vertices[sortIdx])
+
+    def triangulate(self):
+        """Return a list of triangles for the 3 2D points of Nt
+        triangles."""
+
+        if self.n <= 3:
+            return [tuple(self.vertices)]
+
+        result = []
+        remaining = deque(self.vertices)
+
+        # step around the shape and grab ears until only 4 vertices are left
+        while len(remaining) > 4:
+            cross = any(linesCross((remaining[-1], remaining[1]), line)
+                        for line in zip(islice(remaining, 2, len(remaining) - 2),
+                                        islice(remaining, 3, len(remaining) - 1)))
+            if not cross and twiceTriangleArea(remaining[-1], remaining[0], remaining[1]) > 0.:
+                # triangle [-1, 0, 1] is a good one, cut it out
+                result.append((remaining[-1], remaining[0], remaining[1]))
+
+                remaining.popleft()
+            else:
+                remaining.rotate(1)
+
+        # there must now be 0 or 1 concave vertices left; find the
+        # concave vertex (or a vertex) and fan out from it
+        vertices = numpy.array(remaining)
+        shiftedUp = vertices - numpy.roll(vertices, 1, axis=0)
+        shiftedBack = numpy.roll(vertices, -1, axis=0) - vertices
+
+        # signed area for each triangle (i-1, i, i+1) for vertex i
+        areas = shiftedBack[:, 1]*shiftedUp[:, 0] - shiftedUp[:, 1]*shiftedBack[:, 0]
+
+        concave = numpy.where(areas < 0.)[0]
+
+        fan = (concave[0] if len(concave) else 0)
+        remaining.rotate(fan)
+        fan = remaining.popleft()
+        result.extend([(fan, remaining[0], remaining[1]),
+                       (fan, remaining[1], remaining[2])])
+
+        return result
 
 class triangle:
 
