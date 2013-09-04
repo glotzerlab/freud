9ac218e28cfafaa3e8d0cbc567a1ed1d2cfdb19d
harperic <harperic@umich.edu>
2013-09-04 15:47:25 -0400

    Many updates to the complement function. Single match works to match a single point to a single cavity. Thought needs to be put in to how to handle multiple matches and multiple shapes

--- py/freud/complement.py
+++ py/freud/complement.py
@@ -8,30 +8,21 @@ from _freud import complement
 
 class Pair:
     ## Create a Pair object from a list of points, orientations, and shapes
-    def __init__(self, box, positions=None, orientations=None, shapes=None, types=None, ntypes=None):
+    def __init__(self, box, shapes, ntypes, positions=None, orientations=None, types=None):
         self.box = box
+        # shapes is a dict
+        self.shapes = shapes
+        self.ntypes = ntypes
+        self.points = {}
+        self.cavities = {}
+
+    def update(self, positions, orientations, types):
         self.positions = positions
         self.orientations = orientations
         self.types = types
         if len(self.positions) != len(self.orientations):
             raise RuntimeError("length of positions and orientations must be the same")
         self.np = len(self.positions)
-        # shapes is a dict
-        self.shapes = shapes
-        if not ntypes:
-            self.ntypes = len(self.shapes)
-        else:
-            self.ntypes = ntypes
-        self.points = {}
-        self.cavities = {}
-
-    def update(self, positions, orientations):
-        self.positions = positions
-        self.orientations = orientations
-
-    def update_static(self, shapes, types):
-        self.shapes = shapes
-        self.types = types
 
 # points must be set in the same order as their corresponding cavity for perfect match
     def set_points(self, shape, points):
@@ -55,8 +46,8 @@ class Pair:
         self.cavities[shape] = cavity_list
 
     def single_match(self, rmax, ref_type, check_type):
-        if not len(self.shapes) == len(self.cavities):
-            raise RuntimeError("perfect_match can't be called on unequal sized lists")
+        if not len(self.points) == len(self.cavities):
+            raise RuntimeError("single_match can't be called on unequal sized lists")
         smatch = complement(self.box, rmax)
         match_list = numpy.zeros(self.np, dtype=numpy.int32)
         type_list = numpy.zeros(self.np, dtype=numpy.int32)
@@ -67,24 +58,30 @@ class Pair:
             elif t == 'B':
                 type_list[cnt] = 1
             cnt += 1
-        shape_list = numpy.array([self.shapes[i] for i in range(len(self.shapes))])
 
-        if ref_type == A:
-            ref_num = 0
-        elif ref_type == B:
-            ref_num = 1
+        if self.ntypes == 1:
+            shape_list = numpy.array([self.shapes['A']], dtype=numpy.float32)
+        elif self.ntypes == 2:
+            shape_list = numpy.array([self.shapes['A'], self.shapes['B']], dtype=numpy.float32)
+        # shape_list = numpy.array([self.shapes[i] for i in range(len(self.shapes))])
 
-        if check_type == A:
-            check_num = 0
-        elif check_type == B:
-            check_num = 1
+        if ref_type == 'A':
+            ref_num = numpy.array([0], dtype=numpy.int32)
+        elif ref_type == 'B':
+            ref_num = numpy.array([1], dtype=numpy.int32)
 
-        ref_verts = numpy.array(self.points[ref_type])
+        if check_type == 'A':
+            check_num = numpy.array([0], dtype=numpy.int32)
+        elif check_type == 'B':
+            check_num = numpy.array([1], dtype=numpy.int32)
 
-        check_verts = numpy.array(self.points[check_type])
+        ref_verts = numpy.array(self.points[ref_type], dtype=numpy.int32)
 
-        smatch.compute(match_list, self.positions, type_list, self.orientations, self.shapes, ref_num, check_num, ref_verts, check_verts)
+        check_verts = numpy.array(self.cavities[check_type], dtype=numpy.int32)
 
-        self.match_list = match_list
+        smatch.compute(match_list, self.positions, type_list, self.orientations, shape_list, ref_num, check_num, ref_verts, check_verts)
 
-# It will get tricky here as I have to individually go through all shapes and combine their output
+        nmatch = smatch.getNpair()
+
+        return match_list, nmatch
+        # No idea how to work with multi_match right now
