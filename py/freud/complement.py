6b7b6610d2d8e8502a5a1d94bc80556168c18730
harperic <eharp1126@gmail.com>
2013-10-20 10:44:49 -0400

    Code for complementary match.

--- py/freud/complement.py
+++ py/freud/complement.py
@@ -25,4 +25,50 @@ class Pair:
         nmatch = smatch.getNpair()
 
         return match_list, nmatch
-        # No idea how to work with multi_match right now
+    
+    def complementary_match(self, positions, orientations, types,
+                            rmax=1.0, s_dot_target=1.0, s_dot_tol=0.1, c_dot_target=1.0, c_dot_tol=0.1):
+        s_ang = numpy.copy(orientations)
+        c_ang = numpy.copy(orientations)
+        for i in range(len(orientations)):
+            if types[i] == 'A':
+                s_ang[i] = (s_ang[i] + (numpy.pi / 2.0)) % (2.0 * numpy.pi)
+                c_ang[i] = c_ang[i]
+            if types[i] == 'B':
+                s_ang[i] = (s_ang[i] + (numpy.pi / 2.0)) % (2.0 * numpy.pi)
+                c_ang[i] = (c_ang[i] + numpy.pi) % (2.0 * numpy.pi)
+        self.update(positions, s_ang, c_ang)
+        smatch = complement(self.box, rmax, s_dot_target, s_dot_tol, c_dot_target, c_dot_tol)
+        match_list = numpy.zeros(self.np, dtype=numpy.int32)
+        smatch.compute(match_list, self.positions, self.shape_angle, self.comp_angle)
+        nmatch = smatch.getNpair()
+        return match_list, nmatch
+
+    def self_complementary_match(self, positions, orientations, types,
+                            rmax=1.0, s_dot_target=1.0, s_dot_tol=0.1, c_dot_target=1.0, c_dot_tol=0.1):
+        s_ang = numpy.copy(orientations)
+        c_ang = numpy.copy(orientations)
+        for i in range(len(orientations)):
+            if types[i] == 'A':
+                s_ang[i] = (s_ang[i] + (numpy.pi / 2.0)) % (2.0 * numpy.pi)
+                c_ang[i] = c_ang[i]
+            if types[i] == 'B':
+                s_ang[i] = (s_ang[i] + (numpy.pi / 2.0)) % (2.0 * numpy.pi)
+                c_ang[i] = (c_ang[i] + numpy.pi) % (2.0 * numpy.pi)
+        self.update(positions, s_ang, c_ang)
+        match0 = complement(self.box, rmax, s_dot_target, s_dot_tol, c_dot_target, c_dot_tol)
+        match_list0 = numpy.zeros(self.np, dtype=numpy.int32)
+        match0.compute(match_list0, self.positions, self.shape_angle, self.comp_angle)
+        nmatch0 = smatch.getNpair()
+        match1 = complement(self.box, rmax, (-1.0 * s_dot_target), s_dot_tol, c_dot_target, c_dot_tol)
+        match_list1 = numpy.zeros(self.np, dtype=numpy.int32)
+        match1.compute(match_list1, self.positions, self.shape_angle, self.comp_angle)
+        nmatch1 = smatch.getNpair()
+        match_list = numpy.zeros(self.np, dtype=numpy.float32)
+        for i in range(self.np):
+            if ((match_list0[i] == 1) or (match_list1[i] == 1)):
+                match_list[i] = 1
+            else:
+                match_list[i] = 0
+        nmatch = nmatch0 + nmatch1
+        return match_list, nmatch
