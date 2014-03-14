8686d3bccc03201fd4bcb5440426e5f146a04426
harperic <eharp1126@gmail.com>
2014-03-13 20:09:24 -0400

    Added python wrapper. Preserves both the array and delivers the index in a more user friendly way.

--- py/freud/lindemann.py
+++ py/freud/lindemann.py
@@ -1,1 +1,19 @@
-from _freud import Lind
+import numpy
+
+from _freud import setNumThreads
+from _freud import lindemann
+
+class lindex:
+    """docstring for lindex"""
+    def __init__(self, box, rmax, dr):
+        super(lindex, self).__init__()
+        self.box = box
+        self.rmax = rmax
+        self.dr = dr
+        self.lind_handle = lindemann.Lind(self.box, self.rmax, self.dr)
+
+    def compute(self, pos):
+        self.pos = pos
+        self.lind_handle.compute(self.pos))
+        self.lind_array = numpy.copy(self.lind_handle.getLindexArray())
+        self.lindex = numpy.mean(self.lind_array)
