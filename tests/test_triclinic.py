cacf94e1f941635c69f9fc612d4fdb4eac2e203a
Chrisy Du <xiyudu@umich.edu>
2014-10-20 12:39:02 -0400

    modified trajectory.py to read posfile with a triclinic box, updated test_triclinic to test the POS reader

--- tests/test_triclinic.py
+++ tests/test_triclinic.py
@@ -95,6 +95,39 @@ class TestXMLDCD(unittest.TestCase):
         npt.assert_almost_equal(xz, expected_xz, decimal=1, err_msg="XML_XZFail")
         npt.assert_almost_equal(yz, expected_yz, decimal=1, err_msg="XML_YZFail")
 
-  
+class TestPOS(unittest.TestCase):
+    def test_readBoxFrame(self):
+        pos = "triclinic.pos"
+        traj = trajectory.TrajectoryPOS(pos)
+
+        lx = []
+        ly = []
+        lz = []
+        xy = []
+        xz = []
+        yz = []
+        
+        expected_lx = [6.94586, 6.94586, 6.94586, 6.94586, 6.94586, 6.94586, 6.94586, 6.94586, 6.94586, 6.94586]
+        expected_ly = [6.94586, 6.94586, 6.94586, 6.94586, 6.94586, 6.94586, 6.94586, 6.94586, 6.94586, 6.94586]
+        expected_lz = [6.94586, 6.94586, 6.94586, 6.94586, 6.94586, 6.94586, 6.94586, 6.94586, 6.94586, 6.94586]
+        expected_xy = [0.0, 0.00989, 0.01989, 0.02989, 0.03989, 0.04990, 0.05990, 0.06990, 0.07990, 0.08989]
+        expected_xz = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        expected_yz = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] 
+        for f in traj:
+            lx.append(f.box.getLx())
+            ly.append(f.box.getLy())
+            lz.append(f.box.getLz())
+            xy.append(f.box.getTiltFactorXY())
+            xz.append(f.box.getTiltFactorXZ())
+            yz.append(f.box.getTiltFactorYZ())
+
+        npt.assert_almost_equal(lx, expected_lx, decimal=4, err_msg="XML_LxFail")
+        npt.assert_almost_equal(ly, expected_ly, decimal=4, err_msg="XML_LyFail")
+        npt.assert_almost_equal(lz, expected_lz, decimal=4, err_msg="XML_LzFail")
+        npt.assert_almost_equal(xy, expected_xy, decimal=4, err_msg="XML_XYFail")
+        npt.assert_almost_equal(xz, expected_xz, decimal=1, err_msg="XML_XZFail")
+        npt.assert_almost_equal(yz, expected_yz, decimal=1, err_msg="XML_YZFail")
+
+ 
 if __name__ == '__main__':
     unittest.main()
