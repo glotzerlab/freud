0ad48fd25f822e56591721ccd0dd6e6cd85670b9
Eric Harper <harperic@umich.edu>
2014-11-18 00:13:51 -0500

    Changing back to dot product to see if that solved bug. So far it seems to work.

--- cpp/pairing/pairing2D.cc
+++ cpp/pairing/pairing2D.cc
@@ -56,6 +56,7 @@ void pairing::ComputePairing2D(const vec3<float> *points,
     Index2D b_i = Index2D(m_No, m_Np);
     for (unsigned int i = 0; i < m_Np; i++)
         {
+        // if particle is paired already, skip it
         if (m_pair_array[i] != i)
             {
             continue;
@@ -73,15 +74,17 @@ void pairing::ComputePairing2D(const vec3<float> *points,
                 {
                 break;
                 }
+            // m_match_array[i] = 1;
+            // m_match_array[j] = 1;
+            // m_pair_array[i] = j;
+            // m_pair_array[j] = i;
+            // is_paired = true;
             const vec2<float> r_j(points[j].x, points[j].y);
             vec2<float> r_ij(r_j - r_i);
             vec2<float> r_ji(r_i - r_j);
-            vec3<float> delta = vec3<float>(r_ij.x, r_ij.y, 0.0);
-            // float3 wrapped(m_box.wrap(make_float3(r_ij.x, r_ij.y, 0.0)));
-            delta = m_box.wrap(delta);
+            vec3<float> delta = m_box.wrap(vec3<float>(r_ij.x, r_ij.y, 0.0));
             r_ij = vec2<float>(delta.x, delta.y);
-            delta = vec3<float>(r_ji.x, r_ji.y, 0.0);
-            delta = m_box.wrap(delta);
+            delta = m_box.wrap(vec3<float>(r_ji.x, r_ji.y, 0.0));
             r_ji = vec2<float>(delta.x, delta.y);
             float rsq(dot(r_ij, r_ij));
 
@@ -97,10 +100,12 @@ void pairing::ComputePairing2D(const vec3<float> *points,
                 rotmat2<float> my_mat = rotmat2<float>::fromAngle(-orientations[i]);
                 vec2<float> u_ij(r_ij/sqrt(rsq));
                 u_ij = my_mat * u_ij;
+                u_ij = u_ij / sqrt(dot(u_ij, u_ij));
                 my_mat = rotmat2<float>::fromAngle(-orientations[j]);
                 rsq = dot(r_ji, r_ji);
                 vec2<float> u_ji(r_ji/sqrt(rsq));
                 u_ji = my_mat * u_ji;
+                u_ji = u_ji / sqrt(dot(u_ji, u_ji));
 
                 // for each potential complementary orientation for particle i
                 for (unsigned int a=0; a<m_No; a++)
@@ -112,6 +117,7 @@ void pairing::ComputePairing2D(const vec3<float> *points,
                     // generate vectors
                     float theta_ci = comp_orientations[b_i(a, i)];
                     vec2<float> c_i(cosf(theta_ci), sinf(theta_ci));
+                    c_i = c_i / sqrt(dot(c_i, c_i));
 
                     // for each potential complementary orientation for particle j
                     for (unsigned int b=0; b<m_No; b++)
@@ -120,14 +126,21 @@ void pairing::ComputePairing2D(const vec3<float> *points,
                             break;
                         float theta_cj = comp_orientations[b_i(b, j)];
                         vec2<float> c_j(cosf(theta_cj), sinf(theta_cj));
+                        c_j = c_j / sqrt(dot(c_j, c_j));
                         // calculate the dot products
-                        float d_ij = acos(dot(c_i, u_ij));
-                        float d_ji = acos(dot(c_j, u_ji));
+                        // float d_ij = acos(dot(c_i, u_ij));
+                        // float d_ji = acos(dot(c_j, u_ji));
+                        float d_ij = abs(1.0 - dot(c_i, u_ij));
+                        float d_ji = abs(1.0 - dot(c_j, u_ji));
                         // this check assumes that the target angle between the interparticle vector and the complementary
                         // interface is 0. As the nearest neighbor list may use a larger rmax than was initialized, it has
                         // to check again
                         if ((d_ij < m_comp_dot_tol) && (d_ji < m_comp_dot_tol) && (is_paired==false) && (rsq < (m_rmax * m_rmax)))
                             {
+                            // printf("r_ij = %f %f\n", r_ij.x, r_ij.y);
+                            // printf("r_ji = %f %f\n", r_ji.x, r_ji.y);
+                            // printf("d_ij = %f\n", d_ij);
+                            // printf("d_ji = %f\n", d_ji);
                             m_match_array[i] = 1;
                             m_match_array[j] = 1;
                             m_pair_array[i] = j;
