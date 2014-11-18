c141b5f75f9370f04446c4e6a99173b2719af273
Eric Harper <harperic@umich.edu>
2014-11-18 00:55:43 -0500

    Removed the python wrapper for pairing as it was no longer needed (lots of redundant code, and did not help usability). Commented out for Nearest Neighbors for similar reasons.

--- py/freud/locality.py
+++ py/freud/locality.py
@@ -13,68 +13,68 @@ def iterator_link_cell_iter(self):
     return self
 IteratorLinkCell.__iter__ = iterator_link_cell_iter;
 
-class NNeighbors:
-    ## Initialize NearestNeighbors
-    # \param box The simulation box
-    # \param rmax The maximum distance to search for nearest neighbors
-    # \param n The number of nearest neighbors to find
-    def __init__(self,rmax,n):
-        super(NNeighbors, self).__init__()
-        self.rmax = rmax
-        self.n = int(n)
-        self.handle = NearestNeighbors(self.rmax, self.n)
-        self.neighborList = [None] * self.nFrames
-        self.RsqList = [None] * self.nFrames
+# class NNeighbors:
+#     ## Initialize NearestNeighbors
+#     # \param box The simulation box
+#     # \param rmax The maximum distance to search for nearest neighbors
+#     # \param n The number of nearest neighbors to find
+#     def __init__(self,rmax,n):
+#         super(NNeighbors, self).__init__()
+#         self.rmax = rmax
+#         self.n = int(n)
+#         self.handle = NearestNeighbors(self.rmax, self.n)
+#         self.neighborList = [None] * self.nFrames
+#         self.RsqList = [None] * self.nFrames
 
-    # change any parameters
-    def update(box=None,
-               rmax=None,
-               n=None):
-        if box is not None:
-            self.box = box
-        if rmax is not None:
-            self.rmax = rmax
-        if n is not None:
-            self.n = n
-        if not ((box is None) and (rmax is None) and (n is None)):
-            self.handle = NearestNeighbors(self.rmax, self.n)
+#     # change any parameters
+#     def update(box=None,
+#                rmax=None,
+#                n=None):
+#         if box is not None:
+#             self.box = box
+#         if rmax is not None:
+#             self.rmax = rmax
+#         if n is not None:
+#             self.n = n
+#         if not ((box is None) and (rmax is None) and (n is None)):
+#             self.handle = NearestNeighbors(self.rmax, self.n)
 
-    # find nearest neighbors
-    def compute(self,
-                box,
-                ref_pos,
-                pos):
-        self.box = box
-        self.handle.compute(self.box,ref_pos,pos)
-        self.rmax = self.handle.getRMax()
-        self.neighborList = self.handle.getNeighborList()
-        self.neighborList = self.neighborList.reshape((len(ref_pos), self.n))
-        self.RsqList = self.handle.getRsqList()
-        self.RsqList = self.RsqList.reshape((len(ref_pos), self.n))
+#     # find nearest neighbors
+#     def compute(self,
+#                 box,
+#                 ref_pos,
+#                 pos):
+#         self.box = box
+#         self.handle.compute(self.box,ref_pos,pos)
+#         self.rmax = self.handle.getRMax()
+#         self.neighborList = self.handle.getNeighborList()
+#         self.neighborList = self.neighborList.reshape((len(ref_pos), self.n))
+#         self.RsqList = self.handle.getRsqList()
+#         self.RsqList = self.RsqList.reshape((len(ref_pos), self.n))
 
-    # return the nearest neighbors of point idx
-    def neighbors(self,
-                  idx):
-        if self.neighborList is not None:
-            return self.neighborList[idx]
-        else:
-            try:
-                return self.handle.getNeighbors(idx)
-            except:
-                raise RuntimeError("neighbors have not been calculated")
+#     # return the nearest neighbors of point idx
+#     def neighbors(self,
+#                   idx):
+#         if self.neighborList is not None:
+#             return self.neighborList[idx]
+#         else:
+#             try:
+#                 return self.handle.getNeighbors(idx)
+#             except:
+#                 raise RuntimeError("neighbors have not been calculated")
 
-    # get the distance between point idx and its nearest neighbors
-    def rsq(self,
-            idx):
-        if self.RsqList is not None:
-            return self.RsqList[idx]
-        else:
-            try:
-                return self.handle.getRsq(idx)
-            except:
-                raise RuntimeError("neighbors have not been calculated")
+#     # get the distance between point idx and its nearest neighbors
+#     def rsq(self,
+#             idx):
+#         if self.RsqList is not None:
+#             return self.RsqList[idx]
+#         else:
+#             try:
+#                 return self.handle.getRsq(idx)
+#             except:
+#                 raise RuntimeError("neighbors have not been calculated")
 
-    def setRMax(self, rmax):
-        self.rmax=rmax
-        self.handle.setRMax(rmax)
+#     def setRMax(self, rmax):
+#         self.rmax=rmax
+#         self.handle.setRMax(rmax)
 
