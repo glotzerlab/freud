c02760af62e9482d58222deb158770ee05ce3368
Matthew Spellings <mspells@umich.edu>
2014-12-09 10:32:47 -0500

    Added numpy type checking for values arg of CorrelationFunctions
    fixes #27

---
+++ tests/test_correlation.py
@@ -0,0 +1,30 @@
+import unittest
+
+import numpy as np
+import freud
+
+class TestCorrelationFunction(unittest.TestCase):
+    def test_type_check(self):
+        boxlen = 10
+        N = 500
+        rmax, dr = 3, 0.1
+
+        box = freud.trajectory.Box(boxlen, boxlen, boxlen)
+
+        points = np.asarray(np.random.uniform(-boxlen/2, boxlen/2, (N, 3)),
+                            dtype=np.float32)
+        values = np.ones((N,)) + 0j
+        corrfun = freud.density.ComplexCF(rmax, dr)
+
+        try:
+            corrfun.compute(box, points, values, points, values.conj())
+            assert False # should have thrown an exception
+        except TypeError:
+            assert True
+
+        values = np.asarray(values, dtype=np.complex64)
+        corrfun.compute(box, points, values, points, values.conj())
+        assert True
+
+if __name__ == '__main__':
+    unittest.main()
