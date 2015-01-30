b8aa347c68d24b847389230527e9e2f2c84307ac
Richmond <newmanrs@umich.edu>
2015-01-30 17:17:04 -0500

    Fixed rdf_bmark.py

--- benchmarks/rdf_bmark.py
+++ benchmarks/rdf_bmark.py
@@ -21,11 +21,11 @@ trials = 5;
 avg_time = 0;
 
 # warm up
-rdf = density.RDF(box, 5.0, 0.05)
-rdf.compute(points, points);
+rdf = density.RDF( 5.0, 0.05)
+rdf.compute(box, points, points);
 
 start = time.time();
 for trial in range(0,trials):
-    rdf.compute(points, points);
+    rdf.compute(box, points, points);
 end = time.time();
 print("avg time per trial: {}".format((end-start)/float(trials)))
