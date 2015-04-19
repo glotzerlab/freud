4a0e0338ac48d1066f8a83bbbaeba22d159227df
Eric Harper <harperic@umich.edu>
2015-04-18 22:53:18 -0400

    Fixing the pairing code as the math was previously wrong; it is ready for parallelization. uses cross, dot, and atan2 to find the angle difference for pairing.

--- cpp/pairing/pairing2D.cc
+++ cpp/pairing/pairing2D.cc
@@ -56,30 +56,29 @@ void pairing::ComputePairing2D(const vec3<float> *points,
     Index2D b_i = Index2D(m_No, m_Np);
     for (unsigned int i = 0; i < m_Np; i++)
         {
-        // if particle is paired already, skip it
-        if (m_pair_array[i] != i)
-            {
-            continue;
-            }
+        // get the position of particle i
         const vec2<float> r_i(points[i].x, points[i].y);
         bool is_paired = false;
         //loop over neighbors
         locality::NearestNeighbors::iteratorneighbor it = m_nn->iterneighbor(i);
         for (unsigned int j = it.begin(); !it.atEnd(); j = it.next())
             {
-            // need to check to make sure that neither i nor j are paired, as i could become paired in the inner loop
-            // if ((m_pair_array[j] != j) || (m_pair_array[i] != i))
-            if (m_pair_array[i] != i)
+            // once a particle is paired we can stop
+            if (m_match_array[i] != 0)
                 {
                 break;
                 }
+            // get the position of particle j
             const vec2<float> r_j(points[j].x, points[j].y);
+            // find interparticle vectors
             vec2<float> r_ij(r_j - r_i);
             vec2<float> r_ji(r_i - r_j);
+            // wrap into box
             vec3<float> delta = m_box.wrap(vec3<float>(r_ij.x, r_ij.y, 0.0));
             r_ij = vec2<float>(delta.x, delta.y);
             delta = m_box.wrap(vec3<float>(r_ji.x, r_ji.y, 0.0));
             r_ji = vec2<float>(delta.x, delta.y);
+            // find the squared distance for better computational efficiency
             float rsq(dot(r_ij, r_ij));
 
             // will skip same particle
@@ -88,7 +87,7 @@ void pairing::ComputePairing2D(const vec3<float> *points,
                 {
                 // check if the particles are paired
                 // particles are paired if they are the nearest neighbors that have the complementary vector
-                // pointing in the same direction as the interparticle vector
+                // pointing in the same direction as the paired centroid vectorc
 
                 // rotate the unit interparticle vector
                 rotmat2<float> my_mat = rotmat2<float>::fromAngle(-orientations[i]);
@@ -104,6 +103,7 @@ void pairing::ComputePairing2D(const vec3<float> *points,
                 // for each potential complementary orientation for particle i
                 for (unsigned int a=0; a<m_No; a++)
                     {
+                    // break once pair is detected
                     if (is_paired == true)
                         {
                         break;
@@ -116,23 +116,26 @@ void pairing::ComputePairing2D(const vec3<float> *points,
                     // for each potential complementary orientation for particle j
                     for (unsigned int b=0; b<m_No; b++)
                         {
+                        // break once pair is detected
                         if (is_paired == true)
+                            {
                             break;
+                            }
+                        // generate vectors
                         float theta_cj = comp_orientations[b_i(b, j)];
                         vec2<float> c_j(cosf(theta_cj), sinf(theta_cj));
                         c_j = c_j / sqrt(dot(c_j, c_j));
-                        // calculate the dot products
-                        float d_ij = acos(dot(c_i, u_ij));
-                        float d_ji = acos(dot(c_j, u_ji));
+                        // calculate the angle between vectors using cross and dot method
+                        // only use the abs angle (direction is not necessary)
+                        float d_ij = abs(atan2(((c_i.x*u_ij.y)-(c_i.y*u_ij.x)),dot(c_i, u_ij)));
+                        float d_ji = abs(atan2(((c_j.x*u_ji.y)-(c_j.y*u_ji.x)),dot(c_j, u_ji)));
                         // this check assumes that the target angle between the interparticle vector and the complementary
                         // interface is 0. As the nearest neighbor list may use a larger rmax than was initialized, it has
                         // to check again
                         if ((d_ij < m_comp_dot_tol) && (d_ji < m_comp_dot_tol) && (is_paired==false) && (rsq < (m_rmax * m_rmax)))
                             {
                             m_match_array[i] = 1;
-                            m_match_array[j] = 1;
                             m_pair_array[i] = j;
-                            m_pair_array[j] = i;
                             is_paired = true;
                             } // done pairing particle
                         } // done checking all orientations of j
