fe4579acec4adc3fbcc13a7e2b9ddd2ecf365a85
Eric Harper <harperic@umich.edu>
2015-07-20 12:02:57 -0400

    Finishing merge

--- tests/test_pmft.py
+++ tests/test_pmft.py
@@ -8,14 +8,14 @@ class TestBins(unittest.TestCase):
     def test_generateBins(self):
         # parallel.setNumThreads(1)
         maxX = 5.23
-        dx = 0.1
+        nbinsX = 100
         maxY = 5.23
-        dy = 0.1
+        nbinsY = 100
         maxZ = 5.23
-        dz = 0.1
-        nbinsX = int(2 * numpy.floor(maxX / dx))
-        nbinsY = int(2 * numpy.floor(maxX / dy))
-        nbinsZ = int(2 * numpy.floor(maxX / dz))
+        nbinsZ = 100
+        dx = (2.0 * maxX / float(nbinsX))
+        dy = (2.0 * maxY / float(nbinsY))
+        dz = (2.0 * maxZ / float(nbinsZ))
 
         # make sure the radius for each bin is generated correctly
         listX = numpy.zeros(nbinsX, dtype=numpy.float32)
@@ -56,10 +56,10 @@ class TestPMFXY2DAccumulate(unittest.TestCase):
         angles = numpy.array([0.0, 0.0], dtype=numpy.float32)
         maxX = 3.0
         maxY = 3.0
-        dx = 0.1
-        dy = 0.1
-        nbinsX = int(2 * numpy.floor(maxX / dx))
-        nbinsY = int(2 * numpy.floor(maxY / dy))
+        nbinsX = 100
+        nbinsY = 100
+        dx = (2.0 * maxX / float(nbinsX))
+        dy = (2.0 * maxY / float(nbinsY))
         myPMFT = pmft.PMFXY2D(maxX, maxY, nbinsX, nbinsY)
         myPMFT.accumulate(trajectory.Box(boxSize, boxSize, 0, True), points, angles, points, angles)
 
@@ -90,10 +90,10 @@ class TestPMFXY2DAccumulate(unittest.TestCase):
         angles = numpy.array([0.0, 0.0], dtype=numpy.float32)
         maxX = 3.0
         maxY = 3.0
-        dx = 0.1
-        dy = 0.1
-        nbinsX = int(2 * numpy.floor(maxX / dx))
-        nbinsY = int(2 * numpy.floor(maxY / dy))
+        nbinsX = 100
+        nbinsY = 100
+        dx = (2.0 * maxX / float(nbinsX))
+        dy = (2.0 * maxY / float(nbinsY))
         myPMFT = pmft.PMFXY2D(maxX, maxY, nbinsX, nbinsY)
         myPMFT.accumulate(trajectory.Box(boxSize, boxSize, 0, True), points, angles, points, angles)
 
