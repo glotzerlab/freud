aa236f9023ca8ec347c796afbccd25b962c87067
Matthew Spellings <mspells@umich.edu>
2016-05-10 17:00:31 -0400

    Updated test for new LocalDescriptors API

--- tests/test_order_localdescriptors.py
+++ tests/test_order_localdescriptors.py
@@ -2,6 +2,7 @@ import numpy as np
 import numpy.testing as npt
 import freud
 from freud.order import LocalDescriptors
+import freud.parallel;freud.parallel.setNumThreads(1)
 import unittest
 
 class TestLocalDescriptors(unittest.TestCase):
@@ -12,11 +13,9 @@ class TestLocalDescriptors(unittest.TestCase):
 
         box = freud.trajectory.Box(10)
         positions = np.random.uniform(-box.getLx()/2, box.getLx()/2, size=(N, 3)).astype(np.float32)
-        quats = np.zeros((N, 4), dtype=np.float32)
-        quats[:, 0] = 1
 
         comp = LocalDescriptors(box, Nneigh, lmax, .5, True)
-        comp.compute(positions, quats)
+        comp.compute(positions)
 
         sphs = comp.getSph()
 
