d066ef7f18396861a73131714bc0f76a76e737a1
harperic <harperic@umich.edu>
2016-06-08 13:14:11 -0400

    Needed to change conda fix for OS X only

--- CMake/freud/FREUDMacros.cmake
+++ CMake/freud/FREUDMacros.cmake
@@ -7,9 +7,9 @@ endif (APPLE)
 endmacro(fix_tbb_rpath)
 
 macro(fix_conda_python target)
-if (_using_conda)
+if (_using_conda AND APPLE)
 get_filename_component(_python_lib_file ${PYTHON_LIBRARY} NAME)
 add_custom_command(TARGET ${target} POST_BUILD
                           COMMAND install_name_tool ARGS -change ${_python_lib_file} ${PYTHON_LIBRARY} $<TARGET_FILE:${target}>)
-endif ()
+endif (_using_conda AND APPLE)
 endmacro()
