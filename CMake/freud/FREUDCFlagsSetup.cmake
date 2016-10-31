fd45413631851513b08a90ed960329d80ec30eb0
Joshua A. Anderson <joaander@umich.edu>
2016-10-31 14:35:58 -0400

    Port over latest hoomd cflags setup
    This is needed to allow -march=native setting by default and for
    -std=c++11 to not override any user set flags on the command line.

--- CMake/freud/FREUDCFlagsSetup.cmake
+++ CMake/freud/FREUDCFlagsSetup.cmake
@@ -7,65 +7,65 @@
 if(NOT PASSED_FIRST_CONFIGURE)
     message(STATUS "Overriding CMake's default CFLAGS (this should appear only once)")
 
+    ## Allow GCC_ARCH flag to set the -march field
+    if(NOT GCC_ARCH AND "$ENV{GCC_ARCH}" STREQUAL "")
+        set(GCC_ARCH "native")
+        message(STATUS "GCC_ARCH env var not set, setting -march to ${GCC_ARCH}")
+    else()
+        set(GCC_ARCH $ENV{GCC_ARCH})
+        message(STATUS "Found GCC_ARCH env var, setting -march to ${GCC_ARCH}")
+    endif()
+
+
     # default build type is Release when compiling make files
-    if(NOT CMAKE_BUILD_TYPE AND NOT HONOR_GENTOO_FLAGS)
+    if(NOT CMAKE_BUILD_TYPE)
        if(${CMAKE_GENERATOR} STREQUAL "Xcode")
 
        else(${CMAKE_GENERATOR} STREQUAL "Xcode")
             set(CMAKE_BUILD_TYPE "Release" CACHE STRING  "Build type: options are None, Release, Debug, RelWithDebInfo" FORCE)
         endif(${CMAKE_GENERATOR} STREQUAL "Xcode")
-    endif(NOT CMAKE_BUILD_TYPE AND NOT HONOR_GENTOO_FLAGS)
+    endif()
 
-    if(CMAKE_COMPILER_IS_GNUCXX)
-        # special handling to honor gentoo flags
-        if (HONOR_GENTOO_FLAGS)
-        set(CMAKE_CXX_FLAGS_DEBUG "-Wall" CACHE STRING "Flags used by the compiler during debug builds." FORCE)
-        set(CMAKE_CXX_FLAGS_MINSIZEREL "-Wall -DNDEBUG" CACHE STRING "Flags used by the compiler during minimum size release builds." FORCE)
-        set(CMAKE_CXX_FLAGS_RELEASE "-DNDEBUG -Wall" CACHE STRING "Flags used by the compiler during release builds." FORCE)
-        set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-DNDEBUG -Wall" CACHE STRING "Flags used by the compiler during release builds with debug info." FORCE)
+    if(CMAKE_COMPILER_IS_GNUCXX OR "${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
 
-        else (HONOR_GENTOO_FLAGS)
+        set(_common_options "-march=${GCC_ARCH} -Wall -Wno-unknown-pragmas")
+        set(_common_cxx_options "-march=${GCC_ARCH} -Wall -Wno-unknown-pragmas")
+
+        if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang" AND APPLE)
+            set(_common_cxx_options "${_common_cxx_options} -Wno-c++14-extensions")
+        endif()
 
         # default flags for g++
-        set(CMAKE_CXX_FLAGS_DEBUG "-g -Wall -Wno-unknown-pragmas" CACHE STRING "Flags used by the compiler during debug builds." FORCE)
-        set(CMAKE_CXX_FLAGS_MINSIZEREL "-Os -Wall -Wno-unknown-pragmas -DNDEBUG" CACHE STRING "Flags used by the compiler during minimum size release builds." FORCE)
-        set(CMAKE_CXX_FLAGS_RELEASE "-O3 -funroll-loops -ffast-math -DNDEBUG -Wall -Wno-unknown-pragmas" CACHE STRING "Flags used by the compiler during release builds." FORCE)
-        set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-g -O3 -funroll-loops -ffast-math -DNDEBUG -Wall -Wno-unknown-pragmas" CACHE STRING "Flags used by the compiler during release builds with debug info." FORCE)
+        set(CMAKE_C_FLAGS_DEBUG "${_common_options} -g" CACHE STRING "Flags used by the compiler during debug builds." FORCE)
+        set(CMAKE_C_FLAGS_MINSIZEREL "${_common_options} -Os -DNDEBUG" CACHE STRING "Flags used by the compiler during minimum size release builds." FORCE)
+        set(CMAKE_C_FLAGS_RELEASE "${_common_options} -O3 -funroll-loops -DNDEBUG" CACHE STRING "Flags used by the compiler during release builds." FORCE)
+        set(CMAKE_C_FLAGS_RELWITHDEBINFO "${_common_options} -g -O3 -funroll-loops -DNDEBUG" CACHE STRING "Flags used by the compiler during release builds with debug info." FORCE)
 
-        endif (HONOR_GENTOO_FLAGS)
-    elseif(MSVC80)
-        # default flags for visual studio 2005
-        if (CMAKE_CL_64)
-            set(SSE_OPT "")
-        else (CMAKE_CL_64)
-            set(SSE_OPT "/arch:SSE2")
-        endif (CMAKE_CL_64)
-
-        set(CMAKE_CXX_FLAGS "/DWIN32 /D_WINDOWS /W3 /Zm1000 /EHs /GR /MP" CACHE STRING "Flags used by all build types." FORCE)
-        set(CMAKE_CXX_FLAGS_RELEASE "${SSE_OPT} /Oi /Ot /Oy /fp:fast /MD /Ox /Ob2 /D NDEBUG" CACHE STRING "Flags used by the compiler during release builds." FORCE)
-        set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${SSE_OPT} /Oi /Ot /Oy /fp:fast /MD /Zi /Ox /Ob1 /D NDEBUG" CACHE STRING "Flags used by the compiler during release builds with debug info." FORCE)
-        set(CMAKE_CXX_FLAGS_MINSIZEREL "${SSE_OPT} /Oi /Ot /Oy /fp:fast /MD /O1 /Ob1 /D NDEBUG" CACHE STRING "Flags used by the compiler during minimum size release builds." FORCE)
+        set(CMAKE_CXX_FLAGS_DEBUG "${_common_cxx_options} -g" CACHE STRING "Flags used by the compiler during debug builds." FORCE)
+        set(CMAKE_CXX_FLAGS_MINSIZEREL "${_common_cxx_options} -Os -DNDEBUG" CACHE STRING "Flags used by the compiler during minimum size release builds." FORCE)
+        set(CMAKE_CXX_FLAGS_RELEASE "${_common_cxx_options} -O3 -funroll-loops -DNDEBUG" CACHE STRING "Flags used by the compiler during release builds." FORCE)
+        set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${_common_cxx_options} -g -O3 -funroll-loops -DNDEBUG" CACHE STRING "Flags used by the compiler during release builds with debug info." FORCE)
 
     elseif(CMAKE_CXX_COMPILER MATCHES "icpc")
         # default flags for intel
-        set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g" CACHE STRING "Flags used by the compiler during debug builds." FORCE)
-        set(CMAKE_CXX_FLAGS_MINSIZEREL "-Os -DNDEBUG" CACHE STRING "Flags used by the compiler during minimum size release builds." FORCE)
-        set(CMAKE_CXX_FLAGS_RELEASE "-O3 -DNDEBUG" CACHE STRING "Flags used by the compiler during release builds." FORCE)
-        set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-g -O3 -DNDEBUG" CACHE STRING "Flags used by the compiler during release builds with debug info." FORCE)
+        set(CMAKE_C_FLAGS_DEBUG "-xHOST -O0 -g" CACHE STRING "Flags used by the compiler during debug builds." FORCE)
+        set(CMAKE_C_FLAGS_MINSIZEREL "-xHOST -Os -DNDEBUG" CACHE STRING "Flags used by the compiler during minimum size release builds." FORCE)
+        set(CMAKE_C_FLAGS_RELEASE "-xHOST -O3 -DNDEBUG" CACHE STRING "Flags used by the compiler during release builds." FORCE)
+        set(CMAKE_C_FLAGS_RELWITHDEBINFO "-xHOST -g -O3 -DNDEBUG" CACHE STRING "Flags used by the compiler during release builds with debug info." FORCE)
+
+        set(CMAKE_CXX_FLAGS_DEBUG "-xHOST -O0 -g" CACHE STRING "Flags used by the compiler during debug builds." FORCE)
+        set(CMAKE_CXX_FLAGS_MINSIZEREL "-xHOST -Os -DNDEBUG" CACHE STRING "Flags used by the compiler during minimum size release builds." FORCE)
+        set(CMAKE_CXX_FLAGS_RELEASE "-xHOST -O3 -DNDEBUG" CACHE STRING "Flags used by the compiler during release builds." FORCE)
+        set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-xHOST -g -O3 -DNDEBUG" CACHE STRING "Flags used by the compiler during release builds with debug info." FORCE)
 
     else(CMAKE_COMPILER_IS_GNUCXX)
         message(STATUS "No default CXXFLAGS for your compiler, set them manually")
-    endif(CMAKE_COMPILER_IS_GNUCXX)
+    endif()
 
 SET(PASSED_FIRST_CONFIGURE ON CACHE INTERNAL "First configure has run: CXX_FLAGS have had their defaults changed" FORCE)
 endif(NOT PASSED_FIRST_CONFIGURE)
 
-# adding C++11 Flags
-if (${CMAKE_SYSTEM_NAME} MATCHES "DARWIN")
-    set(CMAKE_CXX_FLAGS "-std=c++11" CACHE STRING "Flag used to specify C++11." FORCE)
-else (${CMAKE_SYSTEM_NAME} MATCHES "DARWIN")
-    set(CMAKE_CXX_FLAGS "-std=gnu++11" CACHE STRING "Flag used to specify C++11." FORCE)
-endif (${CMAKE_SYSTEM_NAME} MATCHES "DARWIN")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 
 # disable crazy windows warnings
 if (WIN32)
