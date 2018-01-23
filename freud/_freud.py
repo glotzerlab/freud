1672ecd8199366b5800022f27c990466de8f0cc7
Joshua A. Anderson <joaander@umich.edu>
2018-01-23 13:46:18 -0500

    Warn users when then import freud from the source directory

--- freud/_freud.py
+++ freud/_freud.py
@@ -3,3 +3,11 @@
 
 # This file exists to allow the freud module to import from the source checkout dir
 # for use when building the sphinx documentation.
+print()
+print("******************** WARNING ********************")
+print("You have imported freud from the source directory.")
+print("Freud must be compiled and installed to function.")
+print("Set your PYTHONPATH appropriately and cd to a ")
+print("a different directory before importing freud. ")
+print("******************** WARNING ********************")
+print()
