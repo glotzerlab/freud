dca52bff914f80fa48241d16a196736b791623c9
Bradley Dice <bdice@bradleydice.com>
2018-01-25 12:24:34 -0500

    Removed _freud.py file, added import warning to __init__.py.

--- freud/__init__.py
+++ freud/__init__.py
@@ -2,6 +2,21 @@
 # This file is part of the Freud project, released under the BSD 3-Clause License.
 
 # __init__.py marks this directory as a python module
+
+try:
+    from . import _freud
+except ImportError as e:
+    print("""
+********************* ERROR **********************
+Could not find compiled _freud module. You may
+have imported freud from the source directory.
+Freud must be compiled and installed to function.
+Set your PYTHONPATH appropriately and change to a
+different directory before importing freud.
+********************* ERROR **********************
+""")
+    raise e
+
 from . import parallel
 from . import box
 from . import bond
