9c2cf15faa69aa7bb4d6f7e05a238e2cbc0462ea
Bradley Dice <bdice@bradleydice.com>
2018-01-29 20:31:16 -0500

    Set up ReadTheDocs to use clang and run setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
 import sys
-from subprocess import call
+from subprocess import call, Popen
 import os
+import shutil
 
 def help_text():
-    print("This script must be called with `python setup.py install` or `python setup.py install --force`.")
+    print('This script must be called with `python setup.py install` or `python setup.py install --force`.')
     sys.exit(1)
 
 if len(sys.argv) == 1:
@@ -16,10 +17,28 @@ if len(sys.argv) == 3 and sys.argv[2] != '--force':
 if len(sys.argv) > 3:
     help_text()
 
+#Check if running on ReadTheDocs
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+
+# Remove the build directory if on ReadTheDocs
+if on_rtd and os.path.isdir('build'):
+    shutil.rmtree('build')
+
 # Create and enter the build directory
-call(["mkdir", "-p", "build"])
-os.chdir("build");
+call(['mkdir', '-p', 'build'])
+os.chdir('build');
 
 # Run CMake, make install
-call(["cmake", "../", "-DENABLE_CYTHON=on"])
-call(["make", "install", "-j4"])
+if on_rtd:
+    # ReadTheDocs needs clang since it uses less memory than gcc
+    clang_env = os.environ.copy()
+    clang_env['CC'] = shutil.which('clang')
+    clang_env['CXX'] = shutil.which('clang++')
+    cmake_process = Popen(['cmake', '../'], env=clang_env)
+    if cmake_process.wait() != 0:
+        print('Errors occurred during CMake.')
+        exit(1)
+    call(['make', 'install', '-j1'])
+else:
+    call(['cmake', '../'])
+    call(['make', 'install', '-j4'])
