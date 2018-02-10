eb6287825bb0c4eac4b198317744119b72d1ee72
Bradley Dice <bdice@bradleydice.com>
2018-02-10 15:27:09 -0500

    Removed old test files and moved internal to util.

--- tests/2d/test_vmd.py
+++
@@ -1,9 +0,0 @@
-from freud import trajectory
-
-# test that the VMD loader properly identifies 2D boxes
-
-VMD.evaltcl('mol load hoomd start.xml dcd dump.dcd')
-traj = trajectory.TrajectoryVMD()
-
-for f in traj:
-    print f.box.getLx(), f.box.getLy(), f.box.getLz(), f.box.is2D()
