c41cf5f46ba76ab9b779a0d62a75f33627cb90d5
Vyas <vramasub@umich.edu>
2018-06-11 04:16:31 -0400

    Retain API

--- freud/parallel.py
+++ freud/parallel.py
@@ -3,11 +3,11 @@
 
 import platform
 import re
-from . import _freud
+from ._freud import setNumThreads, _numThreads
 
 if (re.match("flux.", platform.node()) is not None) or (
         re.match("nyx.", platform.node()) is not None):
-    _freud.setNumThreads(1)
+    setNumThreads(1)
 
 
 class NumThreads:
@@ -21,11 +21,11 @@ class NumThreads:
     """
 
     def __init__(self, N=None):
-        self.restore_N = _freud._numThreads
+        self.restore_N = _numThreads
         self.N = N
 
     def __enter__(self):
-        _freud.setNumThreads(self.N)
+        setNumThreads(self.N)
 
     def __exit__(self, *args):
-        _freud.setNumThreads(self.restore_N)
+        setNumThreads(self.restore_N)
