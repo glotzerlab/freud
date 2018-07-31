2273d9c6169b8d86c63ab2f1adbe20ee7864f12c
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2018-07-31 11:16:33 -0400

    Delete bond.py

--- freud/bond.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2010-2018 The Regents of the University of Michigan
-# This file is from the freud project, released under the BSD 3-Clause License.
-
-from ._freud import BondingAnalysis
-from ._freud import BondingR12
-from ._freud import BondingXY2D
-from ._freud import BondingXYT
-from ._freud import BondingXYZ
