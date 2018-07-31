11fb2414634e786e2695e0058af73a9b0b7313ff
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2018-07-31 14:44:57 -0400

    Remove old import warning

--- freud/__init__.py
+++ freud/__init__.py
@@ -1,21 +1,6 @@
 # Copyright (c) 2010-2018 The Regents of the University of Michigan
 # This file is from the freud project, released under the BSD 3-Clause License.
 
-# __init__.py marks this directory as a python module
-
-try:
-    from . import _freud
-except ImportError as e:
-    print("""
-******************** WARNING **********************
-Could not find a compiled _freud module. You may
-have imported freud from the source directory.
-freud must be compiled and installed to function.
-Set your PYTHONPATH appropriately and change to a
-different directory before importing freud.
-******************** WARNING **********************
-""")
-
 from . import parallel
 from . import box
 from . import bond
