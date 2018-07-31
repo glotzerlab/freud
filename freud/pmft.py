e6d819a02be23a900f99e20aba7bafff094d7bcf
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2018-07-31 11:47:49 -0400

    Delete pmft.py

--- freud/pmft.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2010-2018 The Regents of the University of Michigan
-# This file is from the freud project, released under the BSD 3-Clause License.
-
-from ._freud import PMFTR12
-from ._freud import PMFTXYT
-from ._freud import PMFTXY2D
-from ._freud import PMFTXYZ
