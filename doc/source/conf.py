50b63a20e9d2dbda661de7d61104a4a66f37d9dc
Bradley Dice <bdice@bradleydice.com>
2018-08-28 08:48:55 -0400

    Added examples submodule.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -39,6 +39,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
+    'nbsphinx',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
