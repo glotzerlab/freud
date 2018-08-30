4f445b63668cd1be2e47836a86ea90b9431562cc
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2018-08-29 23:24:44 -0400

    Add tests for AngularSeparation and BondOrder (including one that fails but doesn't seem like it should)

--- tests/test_environment_BondOrder.py
+++ tests/test_environment_BondOrder.py
@@ -1,10 +1,15 @@
 import numpy as np
 import freud
 import unittest
+from freud.errors import FreudDeprecationWarning
+import warnings
 import util
 
 
 class TestBondOrder(unittest.TestCase):
+    def setUp(self):
+        warnings.simplefilter("ignore", category=FreudDeprecationWarning)
+
     def test_nonzero_bins(self):
         """Test that there are exactly 12 non-zero bins for a perfect
         FCC structure"""
@@ -12,10 +17,51 @@ class TestBondOrder(unittest.TestCase):
         quats = np.zeros((len(positions), 4), dtype=np.float32)
         quats[:, 0] = 1
 
-        bo = freud.environment.BondOrder(1.5, 0, 12, 6, 6)
-        bo.compute(box, positions, quats, positions, quats)
+        num_neighbors = 12
+        npt = npp = 6
+        bo = freud.environment.BondOrder(1.5, 0, num_neighbors, npt, npp)
 
+        bo.compute(box, positions, quats, positions, quats)
         self.assertEqual(np.sum(bo.bond_order > 0), 12)
+        self.assertEqual(bo.n_bins_theta, npt)
+        self.assertEqual(bo.n_bins_phi, npp)
+
+        self.assertTrue(np.allclose(
+            bo.theta, np.array([(2*i+1)*np.pi/6 for i in range(npt)])))
+        self.assertTrue(np.allclose(
+            bo.phi, np.array([(2*i+1)*np.pi/12 for i in range(npp)])))
+
+        bo.reset()
+        bo.accumulate(box, positions, quats, positions, quats, mode='lbod')
+        self.assertEqual(np.sum(bo.getBondOrder() > 0), 12)
+        self.assertEqual(bo.getNBinsTheta(), npt)
+        self.assertEqual(bo.getNBinsPhi(), npp)
+
+        self.assertTrue(np.allclose(
+            bo.getTheta(), np.array([(2*i+1)*np.pi/6 for i in range(npt)])))
+        self.assertTrue(np.allclose(
+            bo.getPhi(), np.array([(2*i+1)*np.pi/12 for i in range(npp)])))
+
+        bo.compute(box, positions, quats, positions, quats, mode='obcd')
+        self.assertEqual(np.sum(bo.bond_order > 0), 12)
+        self.assertEqual(bo.n_bins_theta, npt)
+        self.assertEqual(bo.n_bins_phi, npp)
+
+        self.assertTrue(np.allclose(
+            bo.theta, np.array([(2*i+1)*np.pi/6 for i in range(npt)])))
+        self.assertTrue(np.allclose(
+            bo.phi, np.array([(2*i+1)*np.pi/12 for i in range(npp)])))
+
+        bo.resetBondOrder()
+        bo.accumulate(box, positions, quats, positions, quats, mode='oocd')
+        self.assertEqual(np.sum(bo.bond_order > 0), 12)
+        self.assertEqual(bo.n_bins_theta, npt)
+        self.assertEqual(bo.n_bins_phi, npp)
+
+        self.assertTrue(np.allclose(
+            bo.theta, np.array([(2*i+1)*np.pi/6 for i in range(npt)])))
+        self.assertTrue(np.allclose(
+            bo.phi, np.array([(2*i+1)*np.pi/12 for i in range(npp)])))
 
 
 if __name__ == '__main__':
