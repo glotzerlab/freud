b920207124837d090d515ffc822e3678b77e2b34
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2018-09-05 18:40:27 -0400

    Add tests for all the spherical harmonic based OPs

--- tests/test_order_LocalQl.py
+++ tests/test_order_LocalQl.py
@@ -25,20 +25,30 @@ class TestLocalQl(unittest.TestCase):
         comp = freud.order.LocalQl(box, 1.5, 6)
 
         comp.compute(positions)
-        assert np.isclose(np.average(comp.Ql), 0.57452422, atol=1e-5)
-        assert np.allclose(comp.Ql, comp.Ql[0])
+        self.assertTrue(np.isclose(np.average(comp.Ql), 0.57452422, atol=1e-5))
+        self.assertTrue(np.allclose(comp.Ql, comp.Ql[0]))
+        self.assertTrue(np.allclose(comp.getQl(), comp.Ql[0]))
 
         comp.computeAve(positions)
-        assert np.isclose(np.average(comp.Ql), 0.57452422, atol=1e-5)
-        assert np.allclose(comp.ave_Ql, comp.ave_Ql[0])
+        self.assertTrue(np.isclose(np.average(comp.Ql), 0.57452422, atol=1e-5))
+        self.assertTrue(np.allclose(comp.ave_Ql, comp.ave_Ql[0]))
+        self.assertTrue(np.allclose(comp.getAveQl(), comp.ave_Ql[0]))
 
         comp.computeNorm(positions)
-        assert np.isclose(np.average(comp.Ql), 0.57452422, atol=1e-5)
-        assert np.allclose(comp.norm_Ql, comp.norm_Ql[0])
+        self.assertTrue(np.isclose(np.average(comp.Ql), 0.57452422, atol=1e-5))
+        self.assertTrue(np.allclose(comp.norm_Ql, comp.norm_Ql[0]))
+        self.assertTrue(np.allclose(comp.getQlNorm(), comp.norm_Ql[0]))
 
         comp.computeAveNorm(positions)
-        assert np.isclose(np.average(comp.Ql), 0.57452422, atol=1e-5)
-        assert np.allclose(comp.ave_norm_Ql, comp.ave_norm_Ql[0])
+        self.assertTrue(np.isclose(np.average(comp.Ql), 0.57452422, atol=1e-5))
+        self.assertTrue(np.allclose(comp.ave_norm_Ql, comp.ave_norm_Ql[0]))
+        self.assertTrue(np.allclose(comp.getQlAveNorm(), comp.ave_norm_Ql[0]))
+
+        self.assertEqual(box, comp.box)
+        self.assertEqual(box, comp.getBox())
+
+        self.assertEqual(len(positions), comp.num_particles)
+        self.assertEqual(len(positions), comp.getNP())
 
 
 class TestLocalQlNear(unittest.TestCase):
@@ -61,20 +71,30 @@ class TestLocalQlNear(unittest.TestCase):
         comp = freud.order.LocalQlNear(box, 1.5, 6, 12)
 
         comp.compute(positions)
-        assert np.isclose(np.average(comp.Ql), 0.57452416, atol=1e-5)
-        assert np.allclose(comp.Ql, comp.Ql[0])
+        self.assertTrue(np.isclose(np.average(comp.Ql), 0.57452416, atol=1e-5))
+        self.assertTrue(np.allclose(comp.Ql, comp.Ql[0]))
+        self.assertTrue(np.allclose(comp.getQl(), comp.Ql[0]))
 
         comp.computeAve(positions)
-        assert np.isclose(np.average(comp.Ql), 0.57452416, atol=1e-5)
-        assert np.allclose(comp.ave_Ql, comp.ave_Ql[0])
+        self.assertTrue(np.isclose(np.average(comp.Ql), 0.57452416, atol=1e-5))
+        self.assertTrue(np.allclose(comp.ave_Ql, comp.ave_Ql[0]))
+        self.assertTrue(np.allclose(comp.getAveQl(), comp.ave_Ql[0]))
 
         comp.computeNorm(positions)
-        assert np.isclose(np.average(comp.Ql), 0.57452416, atol=1e-5)
-        assert np.allclose(comp.norm_Ql, comp.norm_Ql[0])
+        self.assertTrue(np.isclose(np.average(comp.Ql), 0.57452416, atol=1e-5))
+        self.assertTrue(np.allclose(comp.norm_Ql, comp.norm_Ql[0]))
+        self.assertTrue(np.allclose(comp.getQlNorm(), comp.norm_Ql[0]))
 
         comp.computeAveNorm(positions)
-        assert np.isclose(np.average(comp.Ql), 0.57452416, atol=1e-5)
-        assert np.allclose(comp.ave_norm_Ql, comp.ave_norm_Ql[0])
+        self.assertTrue(np.isclose(np.average(comp.Ql), 0.57452416, atol=1e-5))
+        self.assertTrue(np.allclose(comp.ave_norm_Ql, comp.ave_norm_Ql[0]))
+        self.assertTrue(np.allclose(comp.getQlAveNorm(), comp.ave_norm_Ql[0]))
+
+        self.assertEqual(box, comp.box)
+        self.assertEqual(box, comp.getBox())
+
+        self.assertEqual(len(positions), comp.num_particles)
+        self.assertEqual(len(positions), comp.getNP())
 
 
 if __name__ == '__main__':
