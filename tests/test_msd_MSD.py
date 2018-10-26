96d1ddb46120ca35730a2cdb3a25f7ead33d086a
Vyas Ramasubramani <vyas.ramasubramani@gmail.com>
2018-10-26 15:45:06 -0400

    Make MSD into class capable of accumulation

--- tests/test_msd_MSD.py
+++ tests/test_msd_MSD.py
@@ -2,35 +2,45 @@ from __future__ import division
 
 import numpy as np
 import numpy.testing as npt
-from freud import msd
+import freud.msd
 import unittest
 
 
 class TestMSD(unittest.TestCase):
 
-    def test_msd(self):
+    def test_MSD(self):
         """Test correct behavior for various constructor signatures"""
         positions = np.array([[[1, 0, 0]]])
-        self.assertTrue(msd.MSD(positions) == [0])
-        self.assertTrue(msd.MSD(positions, mode='direct') == [0])
+        msd = freud.msd.MSD()
+        msd_direct = freud.msd.MSD(mode='direct')
+        self.assertTrue(msd.accumulate(positions).msd == [0])
+        self.assertTrue(msd_direct.accumulate(positions).msd == [0])
 
         positions = positions.repeat(10, axis=0)
-        npt.assert_array_almost_equal(msd.MSD(positions), 0)
-        npt.assert_array_almost_equal(msd.MSD(positions, mode='direct'), 0)
+        npt.assert_array_almost_equal(msd.compute(positions).msd, 0)
+        npt.assert_array_almost_equal(msd_direct.compute(positions).msd, 0)
 
         positions[:, 0, 0] = np.arange(10)
         npt.assert_array_almost_equal(
-            msd.MSD(positions), np.arange(10)**2, decimal=4)
+            msd.compute(positions).msd, np.arange(10)**2, decimal=4)
         npt.assert_array_almost_equal(
-            msd.MSD(positions, mode='direct'), np.arange(10)**2)
+            msd_direct.compute(positions).msd, np.arange(10)**2)
 
         positions = positions.repeat(2, axis=1)
         positions[:, 1, :] = 0
         npt.assert_array_almost_equal(
-            msd.MSD(positions), np.arange(10)**2/2, decimal=4)
+            msd.compute(positions).msd, np.arange(10)**2/2, decimal=4)
         npt.assert_array_almost_equal(
-            msd.MSD(positions, mode='direct'), np.arange(10)**2/2)
+            msd_direct.compute(positions).msd, np.arange(10)**2/2)
 
+        # Test accumulation
+        msd.reset()
+        msd.accumulate(positions[:, [0], :])
+        msd.accumulate(positions[:, [1], :])
+        npt.assert_array_almost_equal(
+            msd.msd, msd.compute(positions).msd)
+
+        # Test on a lot of random data against a more naive MSD calculation.
         def simple_msd(positions):
             """A naive MSD calculation, used to test."""
             msds = []
@@ -50,8 +60,9 @@ class TestMSD(unittest.TestCase):
         for _ in range(num_tests):
             positions = np.random.rand(10, 10, 3)
             simple = simple_msd(positions)
-            solution = msd.MSD(positions)
+            solution = msd.compute(positions).msd
             npt.assert_array_almost_equal(solution, simple)
+            msd.reset()
 
 
 if __name__ == '__main__':
