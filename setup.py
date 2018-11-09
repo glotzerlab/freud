d2d2211509a7cf96d48faea8b01470def5a31dda
Bradley Dice <bdice@bradleydice.com>
2018-11-09 09:56:03 -0500

    Bump up to version 0.11.4.

--- setup.py
+++ setup.py
@@ -364,7 +364,7 @@ if args.use_cython:
 if platform.system() == 'Darwin':
     os.environ["MACOSX_DEPLOYMENT_TARGET"]= "10.12"
 
-version = '0.11.3'
+version = '0.11.4'
 
 # Read README for PyPI, fallback to short description if it fails.
 desc = 'Perform various analyses of particle simulations.'
