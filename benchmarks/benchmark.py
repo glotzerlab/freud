49dadf57132c4e379b543363b54f837561161d8b
Jin Soo Ihm <jinihm@umich.edu>
2019-05-29 11:04:56 -0400

    Changed comments to doctstring

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -12,53 +12,106 @@ import timeit
 from freud import parallel
 
 
-# Run a given method and print benchmark results
-#
-# The benchmark class sets up and runs benchmarks of given functions and/or
-# methods and provides convenience routines for one time setup and multiple run
-# invocations. Benchmarks may be run with profiling enabled, with simple
-# timing, or with speedup numbers on multiple cores.
-#
-# It provides an N argument to set the benchmark size. Benchmarks that don't
-# use an N can simply ignore it (and the user can set None). When N is present,
-# timing results are normalized by N.
-#
-# Like the unit test module, it is designed to be an overridden class.
-# Users must override run() to run their benchmark.
-# setup() is called once at the start of every benchmark for one time setup.
 class Benchmark(object):
+    """The freud Benchmark class for running benchmarks.
+
+        Run a given method and print benchmark results
+
+        The benchmark class sets up and runs benchmarks of given functions and/or
+        methods and provides convenience routines for one time setup and multiple run
+        invocations. Benchmarks may be run with simple timing,
+        or with speedup numbers on multiple cores.
+
+        It provides an N argument to set the benchmark size. Benchmarks that don't
+        use an N can simply ignore it (and the user can set None). When N is present,
+        timing results are normalized by N.
+
+        Like the unit test module, it is designed to be an overridden class.
+        Users must override `bench_setup()` and `bench_run()` to run their benchmark.
+        `bench_setup()` is called once at the start of every benchmark for one time setup.
+    """  # noqa: E501
     def __init__(self):
+        """Initialization
+
+        The user should override this method with necessary parameters.
+        """
         self.__N = None
         self.__t = 0
 
-    # Override this method to provide one-time setup for a benchmark.
     def bench_setup(self, N):
+        """Setup function for benchmark.
+
+        The user must override this method to provide preliminary materials as member
+        variables to be used in the `bench_run()` method. The runtime of this method is
+        not measured, and is run only once before `bench_run()` is run.
+
+        Args:
+            N (int):
+                Size of the input.
+        """  # noqa: E501
         pass
 
-    # Override this method to run the benchmark.
     def bench_run(self, N):
+        """Run function for benchmark.
+
+        The user must override this method with lines that the user wants
+        to measure the runtime.
+
+        Args:
+            N (int):
+                Size of the input.
+        """
         pass
 
     def bench_run_parallel(self, N, num_threads):
+        """Run `bench_run()` with `num_threads` many threads.
+
+        Args:
+            N (int):
+                Size of the input.
+            num_threads (int):
+                Number of threads.
+        """
         parallel.setNumThreads(num_threads)
         self.bench_run(N)
 
-    # Returns timer instance with overridden setup and run functions.
     def setup_timer(self, N, num_threads):
+        """Helper function to return a timeit.timer instance that uses `bench_setup()`
+        once to setup and measures the time of `bench_run_parallel()` method.
+
+        Args:
+            N (int):
+                Size of the input.
+            num_threads (int):
+                Number of threads.
+        """
         setup = "self.bench_setup(N)"
         stmt = "self.bench_run_parallel(N, num_threads)"
         varmapping = {"self": self, "N": N, "num_threads": num_threads}
         return timeit.Timer(stmt=stmt, setup=setup, globals=varmapping)
 
-    # Perform the benchmark
-    #
-    # \param N Problem passed to setup() and run.
-    # \param number Number of times to call run()
-    # \param print_stats Print stats to stdout
-    # \returns The average time for each call to run()
-    #
     def run_benchmark(self, N=None, number=100, print_stats=False, repeat=1,
                       num_threads=0):
+        """Perform the benchmark
+
+        Args:
+            N (int):
+                Size of the input (Default value = None).
+            number (int):
+                Number of times to call bench_run() in case one run takes
+                too little time to be significant (Default value = 100).
+            print_stats (bool):
+                Print stats to stdout (Default value = False).
+            repeat (int):
+                Number of times to repeat time measurement of
+                bench_run() (Default value = 1).
+            num_threads (int):
+                Number of threads to use. If 0, use all avaliable threads
+                (Default value = 0).
+
+        Returns:
+            int: The minimum time out of `repeat` many calls to `bench_run()`.
+        """
         # initialize timer
         timer = self.setup_timer(N, num_threads)
 
@@ -73,24 +126,30 @@ class Benchmark(object):
 
         return self.__t
 
-    # Print statistics on the last benchmark run
-    #
-    # Statistics are printed to stdout in a human readable form. Stats are
-    # printed for the results of the last call to run_benchmark.
     def print_stats(self):
+        """Print statistics on the last benchmark run
+
+        Statistics are printed to stdout in a human readable form. Stats are
+        printed for the results of the last call to run_benchmark.
+        """
         if self.__N is not None:
             print('{0:8.3f} ms | {1:8.3f} ns per item'.format(
                 self.__t/1e-3, self.__t/self.__N/1e-9))
         else:
             print('{0:8.3f} ms'.format(self.__t/1e-3))
 
-    # Profile a benchmark run
-    #
-    # \param N Problem passed to setup() and run.
-    # \param number Number of times to call run()
-    #
-    # Runs the benchmark and prints out a cProfile trace
     def run_profile(self, N=None, number=100):
+        """Profile a benchmark run
+
+        Args:
+            N (int):
+                Size of the input (Default value = None).
+            number (int):
+                Number of times to call bench_run() in case one run takes
+                too little time to be significant (Default value = 100).
+
+        Runs the benchmark and prints out a cProfile trace
+        """
         # initilize timer
         timer = self.setup_timer(N)
 
@@ -100,18 +159,29 @@ class Benchmark(object):
         s = pstats.Stats("Profile.prof")
         s.strip_dirs().sort_stats("time").print_stats()
 
-    # System size scaling benchmark
-    #
-    # \param N_list list of the N values to execute the problem at
-    # \param number Number of times to call run() at each problem size
-    # \param print_stats Print the stats to stdout
-    # \returns A list of average run() times following N (in seconds)
-    #
-    # \note The size scaling benchmark autoscales number down linearly with
-    #       problem size (down to a minimum of 1).
-    #
     def run_size_scaling_benchmark(self, N_list, number=1000,
                                    print_stats=True, repeat=1):
+        """Problem size scaling benchmark with all available threads.
+
+        The size scaling benchmark autoscales number down linearly with
+        problem size (down to a minimum of 1).
+
+        Args:
+            N_list (list of ints):
+                N values to execute the problem at.
+            number (int):
+                Number of times to call bench_run() in case one run takes
+                too little time to be significant (Default value = 100).
+            print_stats (bool):
+                Print stats to stdout (Default value = False).
+            repeat (int):
+                Number of times to repeat time measurement of
+                bench_run() (Default value = 1).
+
+        Returns:
+            list of float: A list of average run() times
+            following N (in seconds)
+        """
         if len(N_list) == 0:
             raise TypeError('N_list must be iterable')
 
@@ -131,19 +201,30 @@ class Benchmark(object):
 
         return results
 
-    # Thread scaling benchmark
-    #
-    # \param N_list list of the N values to execute the problem at.
-    # \param number Number of times to call run() at each problem size.
-    # \param print_stats Print the stats to stdout.
-    # \returns A numpy array Ncores x len(N_list) with all the per iteration
-    #          timings (in seconds).
-    #
-    # \note The size scaling benchmark autoscales number down linearly with
-    #       problem size (down to a minimum of 1).
-    #
     def run_thread_scaling_benchmark(self, N_list, number=1000,
                                      print_stats=True, repeat=1):
+        """Thread scaling benchmark.
+
+        The size scaling benchmark autoscales number down linearly with
+        problem size (down to a minimum of 1).
+
+        Args:
+            N_list (list of ints):
+                N values to execute the problem at.
+            number (int):
+                Number of times to call bench_run() in case one run takes
+                too little time to be significant (Default value = 100).
+            print_stats (bool):
+                Print stats to stdout (Default value = False).
+            repeat (int):
+                Number of times to repeat time measurement of
+                bench_run() (Default value = 1).
+
+        Returns:
+            :math:`\left(N_{cores}, len(N_{list})\right)` :class:`numpy.ndarray`:
+                All the per iteration timings with respect to the number of
+                cores used (in seconds).
+        """  # noqa: E501
         if len(N_list) == 0:
             raise TypeError('N_list must be iterable')
 
