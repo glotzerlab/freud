2a2d0c1017eee86247cb833431bc8ed4001293f6
Jin Soo Ihm <jinihm@umich.edu>
2019-05-31 15:10:30 -0400

    Edited circleci config to run style check on OSX

--- clang-format-diff-check.sh
+++ clang-format-diff-check.sh
@@ -3,8 +3,10 @@ RETURNVAL=0
 for f in $DIRECTORIES/*
 do
     if ! diff <(clang-format -style=file $f) $f; then
-        echo "$f has wrong formatting"
+        echo "$f has wrong formatting."
         RETURNVAL=1
+    else
+        echo "$f looks fine."
     fi
 done
 exit $RETURNVAL
