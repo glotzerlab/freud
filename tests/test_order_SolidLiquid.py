48dfa63f33b7cb8ee98a7b858b66aee2611cf6f7
Bradley Dice <bdice@bradleydice.com>
2019-09-23 19:24:41 -0400

    Update tests.

--- tests/test_order_SolidLiquid.py
+++ tests/test_order_SolidLiquid.py
@@ -26,14 +26,10 @@ class TestSolidLiquid(unittest.TestCase):
             6, Q_threshold=.7, S_threshold=6)
         comp_no_norm = freud.order.SolidLiquid(
             6, Q_threshold=.3, S_threshold=6, normalize_Q=False)
-        comp_common_neighbors = freud.order.SolidLiquid(
-            6, Q_threshold=.7, S_threshold=6, common_neighbors=True)
 
-        for comp in (comp_default, comp_no_norm, comp_common_neighbors):
+        for comp in (comp_default, comp_no_norm):
             for query_args in (dict(r_max=2.0), dict(num_neighbors=12)):
                 comp.compute(box, positions, query_args=query_args)
-                if comp.normalize_Q:
-                    npt.assert_allclose(comp.Qlij, 1, rtol=1e-5)
                 self.assertEqual(comp.largest_cluster_size, len(positions))
                 self.assertEqual(len(comp.cluster_sizes), 1)
                 self.assertEqual(comp.cluster_sizes[0], len(positions))
@@ -45,17 +41,15 @@ class TestSolidLiquid(unittest.TestCase):
         Q_threshold = 0.7
         S_threshold = 4
         normalize_Q = False
-        common_neighbors = True
 
         comp = freud.order.SolidLiquid(
             sph_l, Q_threshold=Q_threshold, S_threshold=S_threshold,
-            normalize_Q=normalize_Q, common_neighbors=common_neighbors)
+            normalize_Q=normalize_Q)
 
         self.assertEqual(comp.l, sph_l)
         npt.assert_allclose(comp.Q_threshold, Q_threshold)
         npt.assert_allclose(comp.S_threshold, S_threshold)
         self.assertEqual(comp.normalize_Q, normalize_Q)
-        self.assertEqual(comp.common_neighbors, common_neighbors)
 
         with self.assertRaises(AttributeError):
             comp.largest_cluster_size
