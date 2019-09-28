cc89ece37b0d9cac82c2ee7df27c62f623559936
Vyas Ramasubramani <vramasub@umich.edu>
2019-09-28 15:48:21 -0400

    Update SolidLiquid

--- freud/order.pyx
+++ freud/order.pyx
@@ -659,7 +659,7 @@ cdef class SolidLiquid(PairCompute):
         del self.thisptr
 
     @Compute._compute()
-    def compute(self, box, points, neighbors=None):
+    def compute(self, neighbor_query, neighbors=None):
         R"""Compute the order parameter.
 
         Args:
@@ -672,17 +672,16 @@ cdef class SolidLiquid(PairCompute):
                 (Default value = :code:`None`).
         """
         cdef:
-            freud.box.Box b
             freud.locality.NeighborQuery nq
-            freud.locality.NlistptrWrapper nlistptr
+            freud.locality.NeighborList nlist
             freud.locality._QueryArgs qargs
             const float[:, ::1] l_query_points
             unsigned int num_query_points
 
-        b, nq, nlistptr, qargs, l_query_points, num_query_points = \
-            self.preprocess_arguments(box, points, neighbors=neighbors)
+        nq, nlist, qargs, l_query_points, num_query_points = \
+            self.preprocess_arguments_new(neighbor_query, neighbors=neighbors)
 
-        self.thisptr.compute(nlistptr.get_ptr(),
+        self.thisptr.compute(nlist.get_ptr(),
                              nq.get_ptr(),
                              dereference(qargs.thisptr))
 
