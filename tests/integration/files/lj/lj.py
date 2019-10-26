8de82b9cd97fa1cb4211cebbd716e1004accc27e
Bradley Dice <bdice@bradleydice.com>
2019-10-26 07:25:09 -0400

    Add tests for reader integration.

---
+++ tests/integration/files/lj/lj.py
@@ -0,0 +1,24 @@
+import hoomd
+from hoomd import md
+hoomd.context.initialize()
+
+# Create a 10x10x10 simple cubic lattice of particles with type name A
+hoomd.init.create_lattice(
+    unitcell=hoomd.lattice.sc(a=2.0, type_name='A'), n=10)
+
+# Specify Lennard-Jones interactions between particle pairs
+nl = md.nlist.cell()
+lj = md.pair.lj(r_cut=3.0, nlist=nl)
+lj.pair_coeff.set('A', 'A', epsilon=1.0, sigma=1.0)
+
+# Integrate at constant temperature
+md.integrate.mode_standard(dt=0.005)
+hoomd.md.integrate.langevin(group=hoomd.group.all(), kT=1.2, seed=4)
+
+hoomd.run(10e3)
+
+hoomd.dump.dcd('lj.dcd', period=1000, overwrite=True)
+hoomd.dump.gsd('lj.gsd', period=1000, group=hoomd.group.all(), overwrite=True)
+
+# Run for 10,000 time steps
+hoomd.run(10e3)
---
+++ tests/integration/test_reader_integrations.py
@@ -0,0 +1,55 @@
+import numpy as np
+import numpy.testing as npt
+import freud
+import os
+import unittest
+
+
+def _relative_path(*path):
+    return os.path.join(os.path.dirname(__file__), *path)
+
+
+LJ_GSD = _relative_path('files', 'lj', 'lj.gsd')
+LJ_DCD = _relative_path('files', 'lj', 'lj.dcd')
+LJ_RDF = np.load(_relative_path('files', 'lj', 'rdf.npz'))['rdf']
+
+
+class TestReaderIntegrations(unittest.TestCase):
+
+    def run_analyses(self, traj):
+        """Run sample analyses that should work for any particle system."""
+        rdf = freud.density.RDF(bins=100, r_max=5)
+        ql = freud.order.Steinhardt(6)
+        for system in traj:
+            rdf.compute(system, reset=False)
+            ql.compute(system, neighbors={'num_neighbors': 6})
+        npt.assert_allclose(rdf.rdf, LJ_RDF, rtol=1e-5, atol=1e-5)
+
+    def test_mdanalysis_gsd(self):
+        import MDAnalysis
+        reader = MDAnalysis.coordinates.GSD.GSDReader(LJ_GSD)
+        self.run_analyses(reader)
+
+    def test_mdanalysis_dcd(self):
+        import MDAnalysis
+        reader = MDAnalysis.coordinates.DCD.DCDReader(LJ_DCD)
+        self.run_analyses(reader)
+
+    def test_gsd_gsd(self):
+        import gsd.hoomd
+        with gsd.hoomd.open(LJ_GSD, 'rb') as traj:
+            self.run_analyses(traj)
+
+    def test_garnett_gsd(self):
+        import garnett
+        with garnett.read(LJ_GSD) as traj:
+            self.run_analyses(traj)
+
+    def test_garnett_dcd(self):
+        import garnett
+        with garnett.read(LJ_DCD) as traj:
+            self.run_analyses(traj)
+
+
+if __name__ == '__main__':
+    unittest.main()
