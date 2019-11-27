f2a406cc863e392537ed248e3b06a3b5190b47cc
Mike Henry <mikehenry@boisestate.edu>
2019-11-27 13:36:50 -0700

    Update msd.pyx

--- freud/msd.pyx
+++ freud/msd.pyx
@@ -106,8 +106,8 @@ cdef class MSD(_Compute):
 
           Note that while pyFFTW is released under the BSD 3-Clause license,
           the FFTW library is available under either GPL or a commercial
-          license. As a result, if you wish to distribute this module with 
-          pyFFTW in code, your code must also be GPL licensed unless you 
+          license. As a result, if you wish to distribute this module with
+          pyFFTW in code, your code must also be GPL licensed unless you
           purchase a commercial license.
 
     * :code:`'direct'`:
