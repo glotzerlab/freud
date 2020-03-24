8da94f2eda74f25d4caa27969ed621e6c256ee2f
Yezhi Jin <jinyezhi@umich.edu>
2020-03-24 12:31:20 -0400

    projected onto the xy plane

--- freud/diffraction.py
+++ freud/diffraction.py
@@ -16,23 +16,48 @@ def readFile(fname):
 	with garnett.read(fname) as traj:
 		frame = traj[-1]
 		box = freud.box.Box.from_box(frame.box)
-
-	box = freud.box.Box.from_box(box)
-	positions = frame.positions.copy()
+		positions = frame.positions.copy()
 	return box, positions
 
-def main(box, positions):
+def findMaxBoxEdge(boxVectors):
+	lengthBoxVect = np.linalg.norm(boxVectors, axis=0)
+	return max(lengthBoxVect)
 
-	projected_positions = np.zeros(_positions.shape)
-	projected_positions[:, 0] = _positions[:, 1]
-	projected_positions[:, 1] = _positions[:, 2]
-	projected_box = freud.box.Box.square(max(_box.L))
 
+def main(fname):
+	box, positions = readFile(fname)
+
+	boxVectors = np.asarray([[box.Lx, box.Ly * box.xy, box.Lz * box.xz], [0, box.Ly, box.Lz * box.yz], [0, 0, box.Lz]]);
+	maxEdge = findMaxBoxEdge(boxVectors)
+	# dummy = boxVectors[0:2, 0:2] 
+
+	# find two vectors whose area of parellelogram is largest
+	area_01 = np.linalg.det(boxVectors[0:2, 0:2])
+	area_02 = -np.linalg.det(boxVectors[-1:1, 0:2])
+	area_12 = np.linalg.det(boxVectors[1:3, 0:2])
+	
+	# project the box down to xy plane
+	largestIndex = np.argmax([area_01, area_02, area_12])
+	projected_positions = np.zeros(positions.shape)
+	
+	if largestIndex == 0:
+		projected_positions[:, 0] = positions[:, 0]
+		projected_positions[:, 1] = positions[:, 1]
+		projected_box = freud.box.Box.from_box(box)
+		projected_box.is2D = True
+	elif largestIndex == 1:
+		projected_positions[:, 0] = positions[:, 0]
+		projected_positions[:, 1] = positions[:, 2]
+		projected_box = freud.box.Box(Lx=box.Lx, Ly=box.Lz, xy=0.0, xz=0.0, yz=0.0, is2D=True)
+	elif largestIndex == 2:
+		projected_positions[:, 0] = positions[:, 1]
+		projected_positions[:, 1] = positions[:, 2]
+		projected_box = freud.box.Box(Lx=box.Ly, Ly=box.Lz, xy=0.0, xz=0.0, yz=0.0, is2D=True)
 
 	gd = freud.density.GaussianDensity(125, 2, 0.08)
 	gd.compute(projected_box, projected_positions)
 	density = gd.gaussian_density
-	# msd has fft calculation
+	# using numpy fft implmentation, but msd has fft calculation
 	fft = np.fft.fft2(density)
 
 	plt.imshow(density)
