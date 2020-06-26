b19dba3d982497c83ed3a6af6c3d7bddcd397526
Vyas <vramasub@umich.edu>
2020-06-26 11:40:20 -0400

    Delete extra nlist. Resolves #626.

--- cpp/locality/NeighborComputeFunctional.cc
+++ cpp/locality/NeighborComputeFunctional.cc
@@ -15,8 +15,14 @@ NeighborList makeDefaultNlist(const NeighborQuery* nq, const NeighborList* nlist
         auto nqiter(nq->query(query_points, num_query_points, qargs));
         nlist = nqiter->toNeighborList();
     }
+    // Ideally we wouldn't allocate a new NeighborList at all, but we don't want to force
+    // calling code to manage the memory of a raw pointer, so we prefer to return by value
+    // here, and the toNeighborList function has to return a raw pointer to work with the
+    // Cython interface of freud. Therefore, for now we're sticking with this suboptimal
+    // solution until we can refactor these internalsa little more cleanly.
     locality::NeighborList new_nlist = NeighborList(*nlist);
     new_nlist.validate(num_query_points, nq->getNPoints());
+    delete nlist;
     return new_nlist;
 }
 
