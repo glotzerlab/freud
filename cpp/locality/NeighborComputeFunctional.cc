7624e8abb4547e8da44096ac09ed2b44d0f3cb31
Vyas <vramasub@umich.edu>
2020-07-27 15:14:27 -0400

    Improve documentation.

--- cpp/locality/NeighborComputeFunctional.cc
+++ cpp/locality/NeighborComputeFunctional.cc
@@ -17,11 +17,18 @@ NeighborList makeDefaultNlist(const NeighborQuery* nq, const NeighborList* nlist
         nlist = nqiter->toNeighborList();
         requires_delete = true;
     }
-    // Ideally we wouldn't allocate a new NeighborList at all, but we don't want to force
-    // calling code to manage the memory of a raw pointer, so we prefer to return by value
-    // here, and the toNeighborList function has to return a raw pointer to work with the
-    // Cython interface of freud. Therefore, for now we're sticking with this suboptimal
-    // solution until we can refactor these internalsa little more cleanly.
+    // Ideally we wouldn't allocate a new NeighborList at all, but we don't
+    // want to force calling code to manage the memory of a raw pointer, so we
+    // prefer to return by value here. If we returned the nlist directly, the
+    // calling code would have to know whether or not the nlist required
+    // deleting (i.e. by checking for a null nlist internally). Since the
+    // pointer being passed from Cython (if not NULL) will always be a raw
+    // pointer, and toNeighborList must also always return a raw pointer (to
+    // work with the Cython API of freud) we can't wrap the returned nlist in a
+    // smart pointer here (otherwise it will get double freed if it is a
+    // user-provided nlist), so using smart pointers here is also not an
+    // option. Therefore, this solution of making a copy on the stack is the
+    // best option for now.
     locality::NeighborList new_nlist = NeighborList(*nlist);
     new_nlist.validate(num_query_points, nq->getNPoints());
     if (requires_delete)
