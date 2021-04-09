da07b73cd5c8fdc1697cd7f8375ba9052b46e4da
Bradley Dice <bdice@bradleydice.com>
2021-04-09 14:52:10 -0500

    Print debug.

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ import sys
 
 from skbuild import setup as skbuild_setup
 
+print("OS ENVIRONMENT:")
+print(os.environ)
+
 version = "2.5.1"
 
 # Read README for PyPI, fallback to short description if it fails.
