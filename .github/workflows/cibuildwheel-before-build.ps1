e9e75e93fc635a89f9b74f4d2085f4cfcca8cf17
Bradley Dice <bdice@bradleydice.com>
2021-04-14 15:23:06 -0500

    Initial test of Windows builds (still not fully converted to PowerShell).

---
+++ .github/workflows/cibuildwheel-before-build.ps1
@@ -0,0 +1,24 @@
+$PackageDir = $args[0]
+
+# Install a modern version of CMake for compatibility with TBB 2021
+# (manylinux image includes CMake 2.8)
+#pip install cmake
+
+$TBB_VERSION = "2021.2.0"
+$TBB_ZIP = "v${TBB_VERSION}.zip"
+curl -L -O "https://github.com/oneapi-src/oneTBB/archive/refs/tags/${TBB_ZIP}"
+unzip -q "${TBB_ZIP}"
+
+# Move to a hard-coded path (defined by CIBW_ENVIRONMENT)
+mv "oneTBB-${TBB_VERSION}" "${PACKAGE_DIR}/tbb"
+cd "${PACKAGE_DIR}/tbb"
+mkdir -p build
+cd build
+cmake ../ -DTBB_TEST=OFF
+make
+cmake -DCOMPONENT=runtime -P cmake_install.cmake
+cmake -DCOMPONENT=devel -P cmake_install.cmake
+BUILD_DIR=$(dirname $(find . -name vars.sh))
+cd "${BUILD_DIR}"
+source vars.sh
+echo "TBBROOT: ${TBBROOT:-"not found"}"
