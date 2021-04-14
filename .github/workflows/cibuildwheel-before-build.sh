20b0e726f292a85ff3572e953e4a52dc83a7ea1e
Bradley Dice <bdice@bradleydice.com>
2021-04-14 10:27:15 -0500

    Try building from source.

--- .github/workflows/cibuildwheel-before-build.sh
+++ .github/workflows/cibuildwheel-before-build.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
-echo $(pwd)
-if [ -z $1 ]; then
-    echo "A platform (\"lin\" or \"mac\" or \"win\") must be provided as the first argument."
-    exit 1
-fi
+#if [ -z $1 ]; then
+#    echo "A platform (\"lin\" or \"mac\" or \"win\") must be provided as the first argument."
+#    exit 1
+#fi
 
-TBB_PLATFORM=$1
+#TBB_PLATFORM=$1
 TBB_VERSION="2021.2.0"
-if [[ "${TBB_PLATFORM}" == "win" ]]; then
-    TBB_ZIP="oneapi-tbb-${TBB_VERSION}-${TBB_PLATFORM}.zip"
-else
-    TBB_ZIP="oneapi-tbb-${TBB_VERSION}-${TBB_PLATFORM}.tgz"
-fi
-curl -L -O "https://github.com/oneapi-src/oneTBB/releases/download/v${TBB_VERSION}/${TBB_ZIP}"
-tar -zxvf "${TBB_ZIP}"
+#if [[ "${TBB_PLATFORM}" == "win" ]]; then
+#    TBB_ZIP="oneapi-tbb-${TBB_VERSION}-${TBB_PLATFORM}.zip"
+#else
+#    TBB_ZIP="oneapi-tbb-${TBB_VERSION}-${TBB_PLATFORM}.tgz"
+#fi
+TBB_ZIP="v${TBB_VERSION}.zip"
+#curl -L -O "https://github.com/oneapi-src/oneTBB/releases/download/v${TBB_VERSION}/${TBB_ZIP}"
+curl -L -O "https://github.com/oneapi-src/oneTBB/archive/refs/tags/${TBB_ZIP}"
+unzip "${TBB_ZIP}"
 
 # Move to a hard-coded path (defined by CIBW_ENVIRONMENT)
-mv "oneapi-tbb-${TBB_VERSION}" /project/tbb
-source /project/tbb/env/vars.sh
+mv "oneTBB-${TBB_VERSION}" /project/tbb
+cd /project/tbb
+mkdir -p build
+cd build
+cmake ../ -DTBB_TEST=OFF
+make
+cmake -DCOMPONENT=runtime -P cmake_install.cmake
+cmake -DCOMPONENT=devel -P cmake_install.cmake
+BUILD_DIR=$(dirname $(find . -name vars.sh))
+cd "${BUILD_DIR}"
+source vars.sh
 echo "TBBROOT: ${TBBROOT:-"not found"}"
