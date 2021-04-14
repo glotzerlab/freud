0b4c352c6ddc3a369f4b343a4baafbcf8d29b9a3
Bradley Dice <bdice@bradleydice.com>
2021-04-14 15:57:23 -0500

    Fix TBB command, use newer cmake.

--- .github/workflows/cibuildwheel-before-build.ps1
+++ .github/workflows/cibuildwheel-before-build.ps1
@@ -1,15 +1,15 @@
 $PackageDir = $args[0]
 
-# Install a modern version of CMake for compatibility with TBB 2021
-# (manylinux image includes CMake 2.8)
-#pip install cmake
+# Install a modern version of CMake for compatibility with modern Visual Studio
+pip install cmake
 
 $TBB_VERSION = "2021.2.0"
 $TBB_ZIP = "v${TBB_VERSION}.zip"
 Invoke-WebRequest -Uri "https://github.com/oneapi-src/oneTBB/archive/refs/tags/${TBB_ZIP}" -OutFile "${TBB_ZIP}"
+Expand-Archive -Path "${TBB_ZIP}" -DestinationPath .
 
 # Move to a hard-coded path (defined by CIBW_ENVIRONMENT)
-Expand-Archive -Path "${TBB_ZIP}" -DestinationPath "${PACKAGE_DIR}/tbb"
+mv "oneTBB-${TBB_VERSION}" "${PACKAGE_DIR}/tbb"
 cd "${PACKAGE_DIR}/tbb"
 mkdir -p build
 cd build
