a07e070cc5124b34c6a5519159499ca3a7f5fcb3
Bradley Dice <bdice@bradleydice.com>
2021-04-16 11:12:47 -0400

    Clean first.

--- .github/workflows/cibuildwheel-before-build.sh
+++ .github/workflows/cibuildwheel-before-build.sh
@@ -31,6 +31,7 @@ cd "${PACKAGE_DIR}/tbb"
 mkdir -p build
 cd build
 cmake ../ -DTBB_TEST=OFF
-cmake --build .
+cmake --build . --clean-first
 cmake -DCOMPONENT=runtime -P cmake_install.cmake
 cmake -DCOMPONENT=devel -P cmake_install.cmake
+
