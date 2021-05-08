759d5785fd3722a2fd7a91a7392443a1b9647171
Bradley Dice <bdice@bradleydice.com>
2021-05-08 15:34:18 -0500

    Use bin instead of lib.

--- .github/workflows/cibuildwheel-repair-wheel.ps1
+++ .github/workflows/cibuildwheel-repair-wheel.ps1
@@ -4,4 +4,4 @@ $WHEEL = $args[1]
 # Install delvewheel for Windows wheel repairs
 pip install delvewheel
 
-delvewheel repair --add-path "C:/Program Files (x86)/TBB/lib/" -w "${DEST_DIR}" "${WHEEL}"
+delvewheel repair --add-path "C:/Program Files (x86)/TBB/bin/" -w "${DEST_DIR}" "${WHEEL}"
