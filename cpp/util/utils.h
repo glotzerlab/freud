e5230c06ab6cd0292909ffc87e3d0e951edf8d1e
Bradley Dice <bdice@bradleydice.com>
2021-09-18 23:42:29 -0500

    Remove unused Simpson's integration.

--- cpp/util/utils.h
+++ cpp/util/utils.h
@@ -3,7 +3,6 @@
 
 #include <algorithm>
 #include <cmath>
-#include <stdexcept>
 #include <tbb/blocked_range.h>
 #include <tbb/blocked_range2d.h>
 #include <tbb/parallel_for.h>
@@ -39,45 +38,6 @@ template<typename Scalar> inline Scalar sinc(Scalar x)
         return 1;
     }
     return std::sin(x) / x;
-}
-
-//! Simpson's Rule numerical integration
-/*! \param integrand Callable that returns the integrand value for a provided bin index.
-    \param num_bins Number of bins to integrate over. Must be odd.
-    \param dx Step size between bins.
-    \note The integration summation is performed in double-precision regardless of Scalar type.
-*/
-template<typename Integrand> inline double simpson_integrate(Integrand& integrand, size_t num_bins, double dx)
-{
-    if (num_bins % 2 != 1)
-    {
-        // This only implements the easiest case of Simpson's rule.
-        // Even numbers of bins require additional logic.
-        throw std::invalid_argument("The number of integration bins must be odd.");
-    }
-
-    double integral = 0.0;
-
-    // Simpson's rule uses prefactors 1, 4, 2, 4, 2, ..., 4, 1
-    auto simpson_prefactor = [=](size_t bin) {
-        if (bin == 0 || bin == num_bins - 1)
-        {
-            return 1.0;
-        }
-        if (bin % 2 == 0)
-        {
-            return 2.0;
-        }
-        return 4.0;
-    };
-
-    for (size_t bin_index = 0; bin_index < num_bins; bin_index++)
-    {
-        integral += simpson_prefactor(bin_index) * integrand(bin_index);
-    }
-
-    integral *= dx / 3.0;
-    return integral;
 }
 
 //! Wrapper for for-loop to allow the execution in parallel or not.
