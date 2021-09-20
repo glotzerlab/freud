6f6118b4d1f5bec8a7aca04970fc26363fdb8919
Bradley Dice <bdice@bradleydice.com>
2021-09-19 22:43:13 -0500

    Update C++ comments.

--- cpp/diffraction/StaticStructureFactorDirect.cc
+++ cpp/diffraction/StaticStructureFactorDirect.cc
@@ -58,7 +58,7 @@ void StaticStructureFactorDirect::accumulate(const freud::locality::NeighborQuer
                                              const vec3<float>* query_points, unsigned int n_query_points,
                                              unsigned int n_total, bool reuse_box)
 {
-    // Compute k vectors by sampling reciprocal space
+    // Compute k vectors by sampling reciprocal space.
     const auto& box = neighbor_query->getBox();
     if (box.is2D()) {
         throw std::invalid_argument("2D boxes are not currently supported.");
@@ -70,18 +70,18 @@ void StaticStructureFactorDirect::accumulate(const freud::locality::NeighborQuer
         m_k_points = reciprocal_isotropic(box, k_max, k_min, m_max_k_points);
     }
 
-    // The minimum k value of validity is 2 * pi / L, where L is the smallest side length.
+    // The minimum valid k value is 2 * pi / L, where L is the smallest side length.
     const auto box_L = box.getL();
     const auto min_box_length
         = box.is2D() ? std::min(box_L.x, box_L.y) : std::min(box_L.x, std::min(box_L.y, box_L.z));
     m_min_valid_k = std::min(m_min_valid_k, freud::constants::TWO_PI / min_box_length);
 
 
-    // Compute F_k for the points
+    // Compute F_k for the points.
     const auto F_k_points = StaticStructureFactorDirect::compute_F_k(
         neighbor_query->getPoints(), neighbor_query->getNPoints(), n_total, m_k_points);
 
-    // Compute F_k for the query points (if necessary) and compute the product S_k
+    // Compute F_k for the query points (if necessary) and compute the product S_k.
     std::vector<float> S_k_all_points;
     if (query_points != nullptr)
     {
@@ -94,7 +94,7 @@ void StaticStructureFactorDirect::accumulate(const freud::locality::NeighborQuer
         S_k_all_points = StaticStructureFactorDirect::compute_S_k(F_k_points, F_k_points);
     }
 
-    // Bin the S_k values and track the number of k values in each bin
+    // Bin the S_k values and track the number of k values in each bin.
     util::forLoopWrapper(0, m_k_points.size(), [&](size_t begin, size_t end) {
         for (size_t k_index = begin; k_index < end; ++k_index)
         {
@@ -115,8 +115,8 @@ void StaticStructureFactorDirect::reduce()
     m_structure_factor.prepare(axis_size);
 
     // Reduce the bin counts over all threads, then use them to normalize the
-    // structure factor when computing. This computes a "binned mean" over all k
-    // points. Unlike some other methods in freud, no "frame counter" is needed
+    // structure factor when computing. This computes a binned mean over all k
+    // points. Unlike some other methods in freud, no frame counter is needed
     // because the binned mean accounts for accumulation over frames.
     m_histogram.reduceOverThreads(m_local_histograms);
     m_structure_factor.reduceOverThreadsPerBin(
