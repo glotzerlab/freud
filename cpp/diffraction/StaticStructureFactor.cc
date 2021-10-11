d0db2c0bcc8be4855ef7747a89ada94cdeff6a34
Bradley Dice <bdice@bradleydice.com>
2021-10-11 18:56:52 -0500

    Remove child class names from parent class error messages.

--- cpp/diffraction/StaticStructureFactor.cc
+++ cpp/diffraction/StaticStructureFactor.cc
@@ -13,20 +13,20 @@ StaticStructureFactor::StaticStructureFactor(unsigned int bins, float k_max, flo
 {
     if (bins == 0)
     {
-        throw std::invalid_argument("StaticStructureFactorDirect requires a nonzero number of bins.");
+        throw std::invalid_argument("StaticStructureFactor requires a nonzero number of bins.");
     }
     if (k_max <= 0)
     {
-        throw std::invalid_argument("StaticStructureFactorDirect requires k_max to be positive.");
+        throw std::invalid_argument("StaticStructureFactor requires k_max to be positive.");
     }
     if (k_min < 0)
     {
-        throw std::invalid_argument("StaticStructureFactorDirect requires k_min to be non-negative.");
+        throw std::invalid_argument("StaticStructureFactor requires k_min to be non-negative.");
     }
     if (k_max <= k_min)
     {
         throw std::invalid_argument(
-            "StaticStructureFactorDirect requires that k_max must be greater than k_min.");
+            "StaticStructureFactor requires that k_max must be greater than k_min.");
     }
     // Construct the Histogram object that will be used to track the structure factor
     const auto axes = StructureFactorHistogram::Axes {std::make_shared<util::RegularAxis>(bins, k_min, k_max)};
