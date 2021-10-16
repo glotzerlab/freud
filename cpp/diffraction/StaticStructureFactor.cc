64db727df11189bb70caf95bb933c666597aad38
Bradley Dice <bdice@bradleydice.com>
2021-10-16 12:00:51 -0500

    Fix includes.

--- cpp/diffraction/StaticStructureFactor.cc
+++ cpp/diffraction/StaticStructureFactor.cc
@@ -1,11 +1,14 @@
 // Copyright (c) 2010-2020 The Regents of the University of Michigan
 // This file is from the freud project, released under the BSD 3-Clause License.
 
+#include <stdexcept>
+
+#include "Histogram.h"
+#include "StaticStructureFactor.h"
+
 /*! \file StaticStructureFactor.cc
     \brief Base class for structure factor classes.
 */
-
-#include "StaticStructureFactor.h"
 
 namespace freud { namespace diffraction {
 
