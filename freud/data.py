f5137fdb7865446650ca1fa304a6e6eca009545a
Brandon Butler <butlerbr@umich.edu>
2022-06-01 10:39:42 -0400

    doc: clarify behavior of replication in generate_systems
    Co-authored-by: Vyas Ramasubramani <vramasub@umich.edu>

--- freud/data.py
+++ freud/data.py
@@ -80,8 +80,8 @@ class UnitCell:
                     N = np.product(n_repeats)
                 indices = np.repeat(np.arange(len(uc.basis_positions)), N)
 
-        Below is an example of expanding basis position properties such as type
-        to a replicated lattice.
+        Below is an example of expanding basis position properties such as (in
+        this case, types) to a replicated lattice.
 
         Example::
 
