33b89e8a98fd82a5ca0790a1f998d83b4bfd31b8
Tommy Waltmann <tomwalt@umich.edu>
2022-06-07 17:28:20 -0400

    change benchmarker script

--- benchmarks/benchmarker.py
+++ benchmarks/benchmarker.py
@@ -238,7 +238,7 @@ def list_benchmark_modules():
 
     prefix_path = os.path.dirname(__file__)
     modules = glob.glob(os.path.join(prefix_path, "benchmark_*"))
-    modules = [f[len(str(prefix_path)) : -3] for f in modules]
+    modules = [f[len(str(prefix_path)) + 1 : -3] for f in modules]
     return modules
 
 
