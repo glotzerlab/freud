9cdcde7cdc48cdb0d3d6c774a1ce805a410644b5
Tommy Waltmann <tomwalt@umich.edu>
2022-07-13 15:22:28 -0400

    reword documentation

--- freud/diffraction.pyx
+++ freud/diffraction.pyx
@@ -97,16 +97,16 @@ cdef class StaticStructureFactorDebye(_StaticStructureFactor):
     see :cite:`Farrow2009`. Note that the definition requires :math:`S(0) = N`.
 
     .. note::
-        For true 2D systems the formula one should use is different to the one
-        reported above. See this `link
+
+        For 2D systems freud uses the bessel function :math:`J_0` instead of the
+        :math:`\text{sinc}` function in the equation above. See this `link
         <http://www.scik.org/index.php/jmcs/article/viewFile/263/120>`__
         for more information. For users wishing to calculate the structure
-        factor of quasi 2D systems (when a 2D simulation is used to simulate
-        a real system such as particles on a 2D interface or similar) the 3D
-        formula should be used. In these cases the users are advised to convert
-        the 2D box to a 3D box with third dimension coordinates being set to 0.0
-        and length of third dimension being set to larger of the other two
-        before using the method.
+        factor of quasi 2D systems (i.e. a 2D simulation is used to model a real
+        system such as particles on a 2D interface or similar) the 3D
+        formula should be used. In these cases users should use a 3D box with
+        its longest dimension being in the z-direction and particle positions of
+        the form :math:`(x, y, 0)`.
 
     This implementation uses an evenly spaced number of :math:`k` points between
     `k_min`` and ``k_max``. If ``k_min`` is set to 0 (the default behavior), the
