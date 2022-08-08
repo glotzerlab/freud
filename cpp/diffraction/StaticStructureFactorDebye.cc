0f5ae60c4899518874e54e441a03f1daf2efbf0c
Tommy Waltmann <tomwalt@umich.edu>
2022-08-08 10:11:18 -0400

    rename variable

--- cpp/diffraction/StaticStructureFactorDebye.cc
+++ cpp/diffraction/StaticStructureFactorDebye.cc
@@ -87,7 +87,7 @@ void StaticStructureFactorDebye::accumulate(const freud::locality::NeighborQuery
                     // floating point precision errors can cause k to be
                     // slightly negative, and make evaluating the cylindrical
                     // bessel function impossible.
-                    auto abs_k = std::max(float(0.0), k);
+                    auto nonnegative_k = std::max(float(0.0), k);
 
 #ifdef __clang__
                     // clang doesn't support the special math functions in
@@ -95,9 +95,9 @@ void StaticStructureFactorDebye::accumulate(const freud::locality::NeighborQuery
                     // needed because the other library's implementation is
                     // unique only for complex numbers, otherwise it just tries
                     // to call std::cyl_bessel_j.
-                    S_k += std::real(bessel::cyl_j0(std::complex<double>(abs_k * distance)));
+                    S_k += std::real(bessel::cyl_j0(std::complex<double>(nonnegative_k * distance)));
 #else
-                    S_k += std::cyl_bessel_j(0, abs_k * distance);
+                    S_k += std::cyl_bessel_j(0, nonnegative_k * distance);
 #endif
                 }
                 else
