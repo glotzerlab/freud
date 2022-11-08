631b36e99f2cb16379443535b49760394e7578ca
Tommy Waltmann <tomwalt@umich.edu>
2022-11-08 17:04:45 -0500

    add basic test

---
+++ tests/test_filter_filterSANN.py
@@ -0,0 +1,13 @@
+import pytest
+import freud
+
+def test_call_compute():
+    sys = freud.data.make_random_system(10, 10)
+
+    qargs = {"r_max": 2, 'mode': 'ball'}
+
+    sann = freud.locality.FilterSANN()
+    sann.compute(sys, neighbors=qargs)
+
+    sann.filtered_nlist
+    sann.unfiltered_nlist
