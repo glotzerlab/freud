af1da722e15aa323e73889bf1a243d7ef7cdd5d1
Roy-Kid <lijichen365@126.com>
2022-11-30 12:08:38 +0800

    boilerplate for intermediate scattering

---
+++ cpp/diffraction/IntermediateScattering.cc
@@ -0,0 +1,88 @@
+// Copyright (c) 2010-2020 The Regents of the University of Michigan
+// This file is from the freud project, released under the BSD 3-Clause License.
+
+#include "Box.h"
+#include "ManagedArray.h"
+#include "NeighborQuery.h"
+#include "IntermediateScattering.h"
+#include "utils.h"
+
+/*! \file IntermediateScattering.cc
+    \brief Routines for computing intermediate scattering function.
+*/
+
+namespace freud { namespace diffraction {
+
+void IntermediateScattering::accumulate(const freud::locality::NeighborQuery* neighbor_query, const vec3<float>* query_points, unsigned int n_query_points,
+                                             unsigned int n_total)
+{
+    // Compute k vectors by sampling reciprocal space.
+    const auto& box = neighbor_query->getBox();
+    if (box.is2D())
+    {
+        throw std::invalid_argument("2D boxes are not currently supported.");
+    }
+    const auto k_bin_edges = m_structure_factor.getBinEdges()[0];
+    const auto k_min = k_bin_edges.front();
+    const auto k_max = k_bin_edges.back();
+    if ((!box_assigned) || (box != previous_box))
+    {
+        previous_box = box;
+        m_k_points
+            = StaticStructureFactorDirect::reciprocal_isotropic(box, k_max, k_min, m_num_sampled_k_points);
+        box_assigned = true;
+    }
+
+    // The minimum valid k value is 2 * pi / L, where L is the smallest side length.
+    const auto box_L = box.getL();
+    const auto min_box_length
+        = box.is2D() ? std::min(box_L.x, box_L.y) : std::min(box_L.x, std::min(box_L.y, box_L.z));
+    m_min_valid_k = std::min(m_min_valid_k, freud::constants::TWO_PI / min_box_length);
+
+    // Compute self-part
+
+    if (m_first_all) 
+    {
+        m_r0 = neighbor_query.getPoints(); 
+        m_first_call = false;
+    }
+    const auto m_self_part = IntermediateScattering::compute_self(
+        neighbor_query.getPoints(), m_r0, neighbor_query->getNPoints(), n_total, m_k_points
+    )
+
+    // Compute distinct-part
+    const auto m_distinct_part = IntermediateScattering::compute_distinct(
+        neighbor_query.getPoints(), m_r0, neighbor_query->getNPoints(), n_total, m_k_points
+    )
+
+}
+
+std::vector<std::complex<float>>
+IntermediateScattering::compute_self(const vec3<float>* rt, const vec3<float> r0, unsigned int n_points, unsigned int n_total, const std::vector<vec3<float>>& k_points)
+{
+    std::vector<vec3<float>> r_i_t0(n_points);  // rt - r0 element-wisely
+    util::forLoopWrapper(0, n_points, [&](size_t begin, size_t end)) {
+        for (size_t i = begin; i < end; ++i) 
+        {
+            r_i_t0[i] = rt[i] - rt[0];
+        }
+    }
+
+    return IntermediateScattering::compute_F_k(r_i_t0, n_points, n_total, m_k_points);
+    
+}
+
+std::vector<std::complex<float>>
+IntermediateScattering::compute_distinct(const freud::locality::NeighborQuery* neighbor_query, const std::vector<vec3<float>>& k_points)
+{
+    
+    std::vector<vec3<float>> r_ij_t0(/* TODO: Is any possible to get the number of bonds in c++*/);  
+    // or get the all displacement vector
+
+    return IntermediateScattering::compute_F_k(r_ij_t0, n_points, n_total, m_k_points);
+
+}
+
+    }
+
+}
---
+++ cpp/diffraction/IntermediateScattering.h
@@ -0,0 +1,71 @@
+// Copyright (c) 2010-2020 The Regents of the University of Michigan
+// This file is from the freud project, released under the BSD 3-Clause License.
+
+#ifndef INTERMEDIATE_SCATTERING_H
+#define INTERMEDIATE_SCATTERING_H
+
+#include <complex>
+#include <limits>
+#include <vector>
+
+#include "Box.h"
+#include "Histogram.h"
+#include "ManagedArray.h"
+#include "NeighborQuery.h"
+#include "StaticStructureFactorDirect.h"
+
+/*! \file IntermediateScattering.h
+    \brief Routines for computing intermediate scattering function.
+
+    This computes the intermediate scattering function F(k, t) between sets of time-dependent points and
+    query points. First, k-vectors are sampled isotropically, with each radial
+    bin containing an equal density of k points. Next, the k-vectors are used to
+    compute complex scattering amplitudes F(k, t) by summing over the scattering
+    contribution of all particle positions (atomic form factors are assumed to
+    be 1) at every k-vector. The complex conjugate of the scattering amplitudes
+    of the points at each k-vector are multiplied by the scattering amplitudes
+    of the query points at each k-vector. Finally, the results are binned
+    according to their k-vector's magnitude and normalized by the number of
+    samples in each radial bin in k-space.
+
+    Note that k-vectors are in the physics convention, and q-vectors are in the
+    crystallographic convention. These conventions differ by a factor of 2\pi.
+
+    The methods in this class are based on algorithms in the MIT-licensed
+    dynasor package available here:
+    https://dynasor.materialsmodeling.org/
+
+    See also:
+    https://en.wikipedia.org/wiki/Reciprocal_lattice#Arbitrary_collection_of_atoms
+*/
+
+namespace freud { namespace diffraction {
+
+class IntermediateScattering : public StaticStructureFactorDirect
+{
+    using KBinHistogram = util::Histogram<unsigned int>;
+
+public:
+
+    //! Reset the histogram to all zeros
+    void reset() override
+    {
+        m_local_structure_factor.reset();
+        m_local_k_histograms.reset();
+        m_min_valid_k = std::numeric_limits<float>::infinity();
+        m_reduce = true;
+        box_assigned = false;
+    }
+
+
+private:
+
+    // to record the position r0 of the first frame
+    bool m_first_call {true};
+    static std::vector<vec3<float>> m_r0;
+    
+};
+
+}; }; // namespace freud::diffraction
+
+#endif // INTERMEDIATE_SCATTERING_H
