9b3f083cefda591dbfd64537a9419350a9ed754e
Tommy Waltmann <tomwalt@umich.edu>
2022-12-07 16:53:55 -0500

    small optimizations for performance

--- cpp/locality/FilterSANN.cc
+++ cpp/locality/FilterSANN.cc
@@ -11,56 +11,64 @@ void FilterSANN::compute(const NeighborQuery* nq, const vec3<float>* query_point
                          unsigned int num_query_points, const NeighborList* nlist, QueryArgs qargs)
 {
     // make the unfiltered neighborlist from the arguments
-    // test if this copies the neighbor list.
     m_unfiltered_nlist = std::make_shared<NeighborList>(
-        makeDefaultNlist(nq, nlist, query_points, num_query_points, qargs));
+        std::move(makeDefaultNlist(nq, nlist, query_points, num_query_points, qargs))
+    );
 
-    // work with sorted nlist
+    // work with nlist sorted by distance
     NeighborList sorted_nlist(*m_unfiltered_nlist);
     sorted_nlist.sort(true);
 
-    auto sorted_neighbors=sorted_nlist.getNeighbors();
-    auto sorted_dist=sorted_nlist.getDistances();
-    auto sorted_weights=sorted_nlist.getWeights();
-    auto sorted_counts = sorted_nlist.getCounts();
+    const auto& sorted_neighbors = sorted_nlist.getNeighbors();
+    const auto& sorted_dist = sorted_nlist.getDistances();
+    const auto& sorted_weights = sorted_nlist.getWeights();
+    const auto& sorted_counts = sorted_nlist.getCounts();
 
-    //std::vector<NeighborBond> filtered_bonds;
     using BondVector = tbb::enumerable_thread_specific<std::vector<NeighborBond>>;
     BondVector filtered_bonds;
 
-    // use the paralel for wrapper to loop over QP
-    util::forLoopWrapper(0,sorted_nlist.getNumQueryPoints(), [&](size_t begin, size_t end)
+    // parallelize over query_point_index
+    util::forLoopWrapper(0, sorted_nlist.getNumQueryPoints(), [&](size_t begin, size_t end)
     {
-        // am i mising any other local assignments?
+        // grab thread-local vector
         BondVector::reference local_bonds(filtered_bonds.local());
         for (auto i = begin; i < end; i++)
         {
-            unsigned int m=3;
-            unsigned int first_idx = sorted_nlist.find_first_index(i);
-            float sum=0.0;
-            // sum for the first three closest neighbors
-            for (unsigned int j = 0; j < m && j < sorted_counts(i); j++)
+            unsigned int m = 3;
+            const unsigned int num_unfiltered_neighbors = sorted_counts(i);
+            const unsigned int first_idx = sorted_nlist.find_first_index(i);
+            float sum = 0.0;
+
+            // sum for the three closest neighbors
+            for (unsigned int j = 0; j < m && j < num_unfiltered_neighbors; j++)
             {
-                sum += sorted_dist(first_idx + j);
-                local_bonds.emplace_back(i, sorted_neighbors(first_idx + j, 1), sorted_dist(first_idx + j), sorted_weights(first_idx + j));
-            }
-            while (m<sorted_counts(i) && (sum / (float(m) - 2.0)) > sorted_dist(first_idx + m))
-            {
-                sum += sorted_dist(first_idx + m);
-                local_bonds.emplace_back(i, sorted_neighbors(first_idx + m, 1),
-                                                 sorted_dist(first_idx + m), sorted_weights(first_idx + 2));
-                m += 1;
+                const unsigned int neighbor_idx = first_idx + j;
+                sum += sorted_dist(neighbor_idx);
+                local_bonds.emplace_back(i, sorted_neighbors(neighbor_idx, 1),
+                                            sorted_dist(neighbor_idx),
+                                            sorted_weights(neighbor_idx));
             }
 
+            // add neighbors after adding the first three
+            while (m < num_unfiltered_neighbors && (sum / (float(m) - 2.0)) > sorted_dist(first_idx + m))
+            {
+                const unsigned int neighbor_idx = first_idx + m;
+                sum += sorted_dist(neighbor_idx);
+                local_bonds.emplace_back(i, sorted_neighbors(neighbor_idx, 1),
+                                            sorted_dist(neighbor_idx),
+                                            sorted_weights(neighbor_idx));
+                m += 1;
+            }
         }
     });
 
+    // combine thread-local NeighborBond vectors into a single vector
     tbb::flattened2d<BondVector> flat_filtered_bonds = tbb::flatten2d(filtered_bonds);
     std::vector<NeighborBond> sann_bonds(flat_filtered_bonds.begin(), flat_filtered_bonds.end());
+
+    // sort final bonds array by distance
     tbb::parallel_sort(sann_bonds.begin(), sann_bonds.end(), compareNeighborDistance);
     //tbb::parallel_sort(sann_bonds.begin(), sann_bonds.end(), compareNeighborBond);
-
-    // sort by distances after paralel for loop
 
     m_filtered_nlist = std::make_shared<NeighborList>(sann_bonds);
 };
