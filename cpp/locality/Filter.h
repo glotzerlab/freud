1993e39e46431929861ec057715bbcd2b22114fb
Domagoj Fijan <domagoj.fijan@gmail.com>
2023-02-20 14:57:09 -0500

    Refactor Filter class to contain the warning/error of incomplete shell mech

--- cpp/locality/Filter.h
+++ cpp/locality/Filter.h
@@ -1,6 +1,7 @@
 #ifndef __FILTER_H__
 #define __FILTER_H__
 
+#include <iostream>
 #include "NeighborList.h"
 #include "NeighborQuery.h"
 
@@ -19,9 +20,10 @@ namespace freud { namespace locality {
 class Filter
 {
 public:
-    Filter()
+    Filter(bool allow_incomplete_shell)
         : m_unfiltered_nlist(std::make_shared<NeighborList>()),
-          m_filtered_nlist(std::make_shared<NeighborList>())
+          m_filtered_nlist(std::make_shared<NeighborList>()),
+          m_allow_incomplete_shell(allow_incomplete_shell)
     {}
 
     virtual ~Filter() = default;
@@ -45,6 +47,33 @@ protected:
     std::shared_ptr<NeighborList> m_unfiltered_nlist;
     //!< The filtered neighborlist
     std::shared_ptr<NeighborList> m_filtered_nlist;
+    //<! variable that determines if RAD open (True) or RAD closed (False) is computed
+    bool m_allow_incomplete_shell;
+
+    void warnAboutUnfilledNeighborShells(const std::vector<unsigned int>& unfilled_qps) const
+    {
+        std::string indices;
+        for (const auto& idx : unfilled_qps)
+        {
+            if (idx != std::numeric_limits<unsigned int>::max())
+            {
+                indices += std::to_string(idx);
+                indices += ", ";
+            }
+        }
+        indices = indices.substr(0, indices.size() - 2);
+        if (!indices.empty())
+        {
+            std::ostringstream error_str;
+            error_str << "Query point indices " << indices << " do not have full neighbor shells.";
+            if (!m_allow_incomplete_shell)
+            {
+                throw std::runtime_error(error_str.str());
+            }
+            std::cout << "WARNING: " << error_str.str() << std::endl;
+        }
+    }
+
 };
 
 }; }; // namespace freud::locality
