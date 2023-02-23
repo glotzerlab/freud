5be73d58c15b01157a4e032723be5ab2fecb8465
Domagoj Fijan <50439291+DomFijan@users.noreply.github.com>
2023-02-23 15:30:13 -0500

    Remove prints
    Co-authored-by: Tommy Waltmann <53307607+tommy-waltmann@users.noreply.github.com>

--- tests/test_order_Nematic.py
+++ tests/test_order_Nematic.py
@@ -24,8 +24,6 @@ class TestNematicOrder:
         with pytest.raises(AttributeError):
             op_parallel.nematic_tensor
 
-        print(orientations)
-        print(orientations.shape)
         op_parallel.compute(orientations)
 
         # Test access
