be7ab94c46919e7efaf7021acd9eefdd9c50ae90
Tommy Waltmann <tomwalt@umich.edu>
2023-03-21 15:37:39 -0400

    add license headers

--- cpp/locality/FilterRAD.cc
+++ cpp/locality/FilterRAD.cc
@@ -1,3 +1,6 @@
+// Copyright (c) 2010-2023 The Regents of the University of Michigan
+// This file is from the freud project, released under the BSD 3-Clause License.
+
 #include "FilterRAD.h"
 #include "NeighborBond.h"
 #include "NeighborComputeFunctional.h"
