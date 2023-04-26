1681f446d3dff86700f914c05bff03c97f0d76c6
Tommy Waltmann <tomwalt@umich.edu>
2023-04-26 11:06:45 -0400

    drop python 3.6 support

--- .github/workflows/cibuildwheel-before-test.sh
+++ .github/workflows/cibuildwheel-before-test.sh
@@ -6,12 +6,9 @@ fi
 
 PACKAGE_DIR=$1
 
-if [[ $(python --version 2>&1) == *"3.6."* ]]; then
-  # Python 3.6 is only supported with oldest requirements
+if [[ $(python --version 2>&1) == *"3.7."* ]]; then
+  # Python 3.7 is only supported with oldest requirements
   pip install -U -r "${PACKAGE_DIR}/.github/workflows/oldest-test-reqs.txt" --progress-bar=off
-elif [[ $(python --version 2>&1) == *"3.7."* ]]; then
-  # Python 3.7 was dropped by NEP 29 so not compatible with some of the newest dependencies
-  pip install -U -r "${PACKAGE_DIR}/requirements/requirements-test-compatible.txt" --progress-bar=off
 else
   pip install -U -r "${PACKAGE_DIR}/requirements/requirements-test.txt" --progress-bar=off
 fi
--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
         "Topic :: Scientific/Engineering :: Physics",
         "Programming Language :: C++",
         "Programming Language :: Cython",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
@@ -57,7 +56,7 @@ setup(
         "Source Code": "https://github.com/glotzerlab/freud",
         "Issue Tracker": "https://github.com/glotzerlab/freud/issues",
     },
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=[
         "numpy>=1.14",
         "rowan>=1.2.1",
