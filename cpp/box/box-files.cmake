cf7d0a8c2f579cddfee29b7c5b3e6b5b959da9bc
Tommy Waltmann <tomwalt@umich.edu>
2024-06-06 11:46:49 -0400

    establish cmake pattern for linking to libfreud

---
+++ cpp/box/box-files.cmake
@@ -0,0 +1,4 @@
+set(box_headers box/Box.h)
+set(box_sources "")
+
+
--- cpp/parallel/module-parallel.cc
+++ cpp/parallel/module-parallel.cc
@@ -1,11 +1,11 @@
 
-#include <pybind11/pybind11.h>
+#include <nanobind/nanobind.h>
 
 #include "tbb_config.h"
 
 using namespace freud::parallel;
 
-PYBIND11_MODULE(_parallel, m)
+NB_MODULE(_parallel, m)
 {
     m.def("setNumThreads", &setNumThreads);
 }
