dd7ddeb31715aa26b26de75e22e0103ac7fbbdd7
Joshua A. Anderson <joaander@umich.edu>
2024-06-24 12:25:39 -0400

    Fix compile errors.

--- cpp/box/export_Box.cc
+++ cpp/box/export_Box.cc
@@ -11,8 +11,8 @@ void makeAbsolute(const std::shared_ptr<Box>& box, nb_array<float, nb::shape<-1,
                   nb_array<float, nb::shape<-1, 3>> out)
 {
     unsigned int Nvecs = vecs.shape(0);
-    auto vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
-    auto out_data = reinterpret_cast<vec3<float>*>(out.data());
+    auto* vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
+    auto* out_data = reinterpret_cast<vec3<float>*>(out.data());
     box->makeAbsolute(vecs_data, Nvecs, out_data);
 }
 
@@ -20,8 +20,8 @@ void makeFractional(const std::shared_ptr<Box>& box, nb_array<float, nb::shape<-
                     nb_array<float, nb::shape<-1, 3>> out)
 {
     unsigned int Nvecs = vecs.shape(0);
-    auto vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
-    auto out_data = reinterpret_cast<vec3<float>*>(out.data());
+    auto* vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
+    auto* out_data = reinterpret_cast<vec3<float>*>(out.data());
     box->makeFractional(vecs_data, Nvecs, out_data);
 }
 
@@ -29,8 +29,8 @@ void getImages(const std::shared_ptr<Box>& box, nb_array<float, nb::shape<-1, 3>
                nb_array<int, nb::shape<-1, 3>> images)
 {
     const unsigned int Nvecs = vecs.shape(0);
-    auto vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
-    auto images_data = reinterpret_cast<vec3<int>*>(images.data());
+    auto* vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
+    auto* images_data = reinterpret_cast<vec3<int>*>(images.data());
     box->getImages(vecs_data, Nvecs, images_data);
 }
 
@@ -38,17 +38,17 @@ void wrap(const std::shared_ptr<Box>& box, nb_array<float, nb::shape<-1, 3>> vec
           nb_array<float, nb::shape<-1, 3>> out)
 {
     const unsigned int Nvecs = vecs.shape(0);
-    auto vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
-    auto out_data = reinterpret_cast<vec3<float>*>(out.data());
+    auto* vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
+    auto* out_data = reinterpret_cast<vec3<float>*>(out.data());
     box->wrap(vecs_data, Nvecs, out_data);
 }
 
 void unwrap(const std::shared_ptr<Box>& box, nb_array<float> vecs, nb_array<int> images, nb_array<float> out)
 {
     const unsigned int Nvecs = vecs.shape(0);
-    auto vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
-    auto images_data = reinterpret_cast<vec3<int>*>(images.data());
-    auto out_data = reinterpret_cast<vec3<float>*>(out.data());
+    auto* vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
+    auto* images_data = reinterpret_cast<vec3<int>*>(images.data());
+    auto* out_data = reinterpret_cast<vec3<float>*>(out.data());
     box->unwrap(vecs_data, images_data, Nvecs, out_data);
 }
 
@@ -56,8 +56,8 @@ std::vector<float> centerOfMass(const std::shared_ptr<Box>& box, nb_array<float>
                                 nb_array<float, nb::shape<-1>> masses)
 {
     const unsigned int Nvecs = vecs.shape(0);
-    auto vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
-    auto masses_data = reinterpret_cast<float*>(masses.data());
+    auto* vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
+    auto* masses_data = reinterpret_cast<float*>(masses.data());
     auto com = box->centerOfMass(vecs_data, Nvecs, masses_data);
     return {com.x, com.y, com.z};
 }
@@ -65,8 +65,8 @@ std::vector<float> centerOfMass(const std::shared_ptr<Box>& box, nb_array<float>
 void center(const std::shared_ptr<Box>& box, nb_array<float> vecs, nb_array<float, nb::ndim<1>> masses)
 {
     const unsigned int Nvecs = vecs.shape(0);
-    auto vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
-    auto masses_data = reinterpret_cast<float*>(masses.data());
+    auto* vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
+    auto* masses_data = reinterpret_cast<float*>(masses.data());
     box->center(vecs_data, Nvecs, masses_data);
 }
 
@@ -74,10 +74,10 @@ void computeDistances(const std::shared_ptr<Box>& box, nb_array<float> query_poi
                       nb_array<float, nb::ndim<1>> distances)
 {
     const unsigned int n_query_points = query_points.shape(0);
-    auto query_points_data = reinterpret_cast<vec3<float>*>(query_points.data());
+    auto* query_points_data = reinterpret_cast<vec3<float>*>(query_points.data());
     const unsigned int n_points = points.shape(0);
-    auto points_data = reinterpret_cast<vec3<float>*>(points.data());
-    auto distances_data = reinterpret_cast<float*>(distances.data());
+    auto* points_data = reinterpret_cast<vec3<float>*>(points.data());
+    auto* distances_data = reinterpret_cast<float*>(distances.data());
     if (n_query_points != n_points)
     {
         throw std::invalid_argument("The number of query points and points must match.");
@@ -89,10 +89,10 @@ void computeAllDistances(const std::shared_ptr<Box>& box, nb_array<float> query_
                          nb_array<float> points, nb_array<float, nb::ndim<2>> distances)
 {
     const unsigned int n_query_points = query_points.shape(0);
-    auto query_points_data = reinterpret_cast<vec3<float>*>(query_points.data());
+    auto* query_points_data = reinterpret_cast<vec3<float>*>(query_points.data());
     const unsigned int n_points = points.shape(0);
-    auto points_data = reinterpret_cast<vec3<float>*>(points.data());
-    auto distances_data = reinterpret_cast<float*>(distances.data());
+    auto* points_data = reinterpret_cast<vec3<float>*>(points.data());
+    auto* distances_data = reinterpret_cast<float*>(distances.data());
     box->computeAllDistances(query_points_data, n_query_points, points_data, n_points, distances_data);
 }
 
@@ -100,8 +100,8 @@ void contains(const std::shared_ptr<Box>& box, nb_array<float> points,
               nb_array<bool, nb::ndim<1>> contains_mask)
 {
     const unsigned int n_points = points.shape(0);
-    auto points_data = reinterpret_cast<vec3<float>*>(points.data());
-    auto contains_mask_data = reinterpret_cast<bool*>(contains_mask.data());
+    auto* points_data = reinterpret_cast<vec3<float>*>(points.data());
+    auto* contains_mask_data = reinterpret_cast<bool*>(contains_mask.data());
     box->contains(points_data, n_points, contains_mask_data);
 }
 
