2d51bd613e8dc481269c8de3ff594c34eaaee627
Tommy Waltmann <tomwalt@umich.edu>
2024-07-17 09:40:32 -0400

    cmake boilerplate code

---
+++ cpp/pmft/module-pmft.cc
@@ -0,0 +1,14 @@
+#include <nanobind/nanobind.h>
+
+#include "export_PMFT.h"
+
+using namespace freud::pmft::detail
+
+NB_MODULE(_pmft, m)
+{
+    //export_PMFT(m);
+    //export_PMFTR12(m);
+    //export_PMFTXY(m);
+    //export_PMFTXYT(m);
+    //export_PMFTXYZ(m);
+}
