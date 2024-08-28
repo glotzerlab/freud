c684f10e64f565bd51dbd382a18a8663880849ca
Joshua A. Anderson <joaander@umich.edu>
2024-08-28 12:47:54 -0400

    Port PMFTR12

--- cpp/pmft/pmft-files.cmake
+++ cpp/pmft/pmft-files.cmake
@@ -1,5 +1,5 @@
-set(pmft_sources pmft/PMFTXY.cc pmft/PMFTXYZ.cc)
-# pmft/PMFTR12.cc pmft/PMFTXYT.cc)
+set(pmft_sources pmft/PMFTXY.cc pmft/PMFTXYZ.cc pmft/PMFTR12.cc)
+# pmft/PMFTXYT.cc)
 
-set(pmft_headers pmft/PMFT.h pmft/PMFTXY.h pmft/PMFTXYZ.h)
-# pmft/PMFTR12.h pmft/PMFTXYT.h)
+set(pmft_headers pmft/PMFT.h pmft/PMFTXY.h pmft/PMFTXYZ.h pmft/PMFTR12.h)
+# pmft/PMFTXYT.h)
