d2d5410926bad0bf20dcc9ec8abb51dafb933480
Joshua A. Anderson <joaander@umich.edu>
2024-09-03 13:44:05 -0400

    box passes clang-tidy

--- freud/box/export-Box.cc
+++ freud/box/export-Box.cc
@@ -3,32 +3,34 @@
 
 #include <memory>
 #include <stdexcept>
+#include <vector>
 
 #include "Box.h"
 #include "export-Box.h"
 #include "VectorMath.h"
+#include "nanobind/ndarray.h"
 
 namespace nb = nanobind;
 
 namespace freud { namespace box { namespace wrap {
 
-void makeAbsolute(const std::shared_ptr<Box>& box, nb_array<float> vecs, nb_array<float> out)
+void makeAbsolute(const std::shared_ptr<Box>& box, const nb_array<float>& vecs, const nb_array<float>& out)
 {
-    unsigned int Nvecs = vecs.shape(0);
+    unsigned int const Nvecs = vecs.shape(0);
     auto* vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
     auto* out_data = reinterpret_cast<vec3<float>*>(out.data());
     box->makeAbsolute(vecs_data, Nvecs, out_data);
 }
 
-void makeFractional(const std::shared_ptr<Box>& box, nb_array<float> vecs, nb_array<float> out)
+void makeFractional(const std::shared_ptr<Box>& box, const nb_array<float>& vecs, const nb_array<float>& out)
 {
-    unsigned int Nvecs = vecs.shape(0);
+    unsigned int const Nvecs = vecs.shape(0);
     auto* vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
     auto* out_data = reinterpret_cast<vec3<float>*>(out.data());
     box->makeFractional(vecs_data, Nvecs, out_data);
 }
 
-void getImages(const std::shared_ptr<Box>& box, nb_array<float> vecs, nb_array<int> images)
+void getImages(const std::shared_ptr<Box>& box, const nb_array<float>& vecs, const nb_array<int>& images)
 {
     const unsigned int Nvecs = vecs.shape(0);
     auto* vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
@@ -36,7 +38,7 @@ void getImages(const std::shared_ptr<Box>& box, nb_array<float> vecs, nb_array<i
     box->getImages(vecs_data, Nvecs, images_data);
 }
 
-void wrap(const std::shared_ptr<Box>& box, nb_array<float> vecs, nb_array<float> out)
+void wrap(const std::shared_ptr<Box>& box, const nb_array<float>& vecs, const nb_array<float>& out)
 {
     const unsigned int Nvecs = vecs.shape(0);
     auto* vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
@@ -44,7 +46,7 @@ void wrap(const std::shared_ptr<Box>& box, nb_array<float> vecs, nb_array<float>
     box->wrap(vecs_data, Nvecs, out_data);
 }
 
-void unwrap(const std::shared_ptr<Box>& box, nb_array<float> vecs, nb_array<int> images, nb_array<float> out)
+void unwrap(const std::shared_ptr<Box>& box, const nb_array<float>& vecs, const nb_array<int>& images, const nb_array<float>& out)
 {
     const unsigned int Nvecs = vecs.shape(0);
     auto* vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
@@ -53,8 +55,8 @@ void unwrap(const std::shared_ptr<Box>& box, nb_array<float> vecs, nb_array<int>
     box->unwrap(vecs_data, images_data, Nvecs, out_data);
 }
 
-std::vector<float> centerOfMass(const std::shared_ptr<Box>& box, nb_array<float> vecs,
-                                nb_array<float, nb::shape<-1>> masses)
+std::vector<float> centerOfMass(const std::shared_ptr<Box>& box, const nb_array<float>& vecs,
+                                const nb_array<float, nb::shape<-1>>& masses)
 {
     const unsigned int Nvecs = vecs.shape(0);
     auto* vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
@@ -63,7 +65,7 @@ std::vector<float> centerOfMass(const std::shared_ptr<Box>& box, nb_array<float>
     return {com.x, com.y, com.z};
 }
 
-void center(const std::shared_ptr<Box>& box, nb_array<float> vecs, nb_array<float, nb::ndim<1>> masses)
+void center(const std::shared_ptr<Box>& box, const nb_array<float>& vecs, const nb_array<float, nb::ndim<1>>& masses)
 {
     const unsigned int Nvecs = vecs.shape(0);
     auto* vecs_data = reinterpret_cast<vec3<float>*>(vecs.data());
@@ -71,8 +73,8 @@ void center(const std::shared_ptr<Box>& box, nb_array<float> vecs, nb_array<floa
     box->center(vecs_data, Nvecs, masses_data);
 }
 
-void computeDistances(const std::shared_ptr<Box>& box, nb_array<float> query_points, nb_array<float> points,
-                      nb_array<float, nb::ndim<1>> distances)
+void computeDistances(const std::shared_ptr<Box>& box, const nb_array<float>& query_points, const nb_array<float>& points,
+                      const nb_array<float, nb::ndim<1>>& distances)
 {
     const unsigned int n_query_points = query_points.shape(0);
     auto* query_points_data = reinterpret_cast<vec3<float>*>(query_points.data());
@@ -86,8 +88,8 @@ void computeDistances(const std::shared_ptr<Box>& box, nb_array<float> query_poi
     box->computeDistances(query_points_data, n_query_points, points_data, distances_data);
 }
 
-void computeAllDistances(const std::shared_ptr<Box>& box, nb_array<float> query_points,
-                         nb_array<float> points, nb_array<float, nb::ndim<2>> distances)
+void computeAllDistances(const std::shared_ptr<Box>& box, const nb_array<float>& query_points,
+                         const nb_array<float>& points, const nb_array<float, nb::ndim<2>>& distances)
 {
     const unsigned int n_query_points = query_points.shape(0);
     auto* query_points_data = reinterpret_cast<vec3<float>*>(query_points.data());
@@ -97,8 +99,8 @@ void computeAllDistances(const std::shared_ptr<Box>& box, nb_array<float> query_
     box->computeAllDistances(query_points_data, n_query_points, points_data, n_points, distances_data);
 }
 
-void contains(const std::shared_ptr<Box>& box, nb_array<float> points,
-              nb_array<bool, nb::ndim<1>> contains_mask)
+void contains(const std::shared_ptr<Box>& box, const nb_array<float>& points,
+              const nb_array<bool, nb::ndim<1>>& contains_mask)
 {
     const unsigned int n_points = points.shape(0);
     auto* points_data = reinterpret_cast<vec3<float>*>(points.data());
