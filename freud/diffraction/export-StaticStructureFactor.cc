4bf79430cf801ac237593d968d194952c10f52e7
Maria Ward Rashidi <mariarw@umich.edu>
2024-09-17 13:32:18 -0400

    added missing methods and fixed some formatting

--- freud/diffraction/export-StaticStructureFactor.cc
+++ freud/diffraction/export-StaticStructureFactor.cc
@@ -16,12 +16,11 @@ namespace freud { namespace diffraction {
 
         void accumulate(std::shared_ptr<StaticStructureFactor> self,
                         std::shared_ptr<locality::NeighborQuery> neighbor_query,
-                        // const vec3<float>* query_points,
                         nb_array<float, nanobind::shape<-1, 3>> query_points,
-                        unsigned int n_query_points,
                         unsigned int n_total)
         {
             auto* query_points_data = reinterpret_cast<vec3<float>*>(query_points.data());
+            unsigned int n_query_points = query_points.shape(0);
             self->accumulate(neighbor_query, query_points_data, n_query_points, n_total);
         };
 
@@ -32,13 +31,14 @@ namespace freud { namespace diffraction {
         void export_StaticStructureFactor(nanobind::module_& m)
         {
             nanobind::class_<StaticStructureFactor>(m, "StaticStructureFactor")
-                // .def(nanobind::init<unsigned int, float, float>())
                 .def("accumulate", &wrap::accumulate, nanobind::arg("neighbor_query"),
-                    nanobind::arg("query_points"), nanobind::arg("n_query_points"),
+                    nanobind::arg("query_points"),
                     nanobind::arg("n_total"))
                 .def("reset", &StaticStructureFactor::reset)
                 .def("getStructureFactor", &StaticStructureFactor::getStructureFactor)
-                .def("getMinValidK", &StaticStructureFactor::getMinValidK);
+                .def("getMinValidK", &StaticStructureFactor::getMinValidK)
+                .def("getBinCenters", &StaticStructureFactor::getBinCenters)
+                .def("getBinEdges", &StaticStructureFactor::getBinEdges);
         }
 
     } // namespace detail
