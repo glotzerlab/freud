1bb421f0ecb3caf01575f647cf53c3d74543afbc
Joshua A. Anderson <joaander@umich.edu>
2025-02-04 10:24:44 -0500

    Extend copyright through 2025

--- freud/order/Steinhardt.cc
+++ freud/order/Steinhardt.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010-2024 The Regents of the University of Michigan
+// Copyright (c) 2010-2025 The Regents of the University of Michigan
 // This file is from the freud project, released under the BSD 3-Clause License.
 
 #include "Steinhardt.h"
