68dadcfb3a5338d9f482c06e4ae0a448d53b807f
Joshua A. Anderson <joaander@umich.edu>
2025-02-10 09:04:12 -0500

    Remove duplicate export.

--- freud/util/export-ManagedArray.cc
+++ freud/util/export-ManagedArray.cc
@@ -13,7 +13,6 @@ void export_ManagedArray(nanobind::module_& module)
 { // python wrapper classes for ManagedArray
     export_ManagedArray<float>(module, "ManagedArray_float");
     export_ManagedArray<double>(module, "ManagedArray_double");
-    export_ManagedArray<std::complex<float>>(module, "ManagedArray_complexfloat");
     export_ManagedArray<unsigned int>(module, "ManagedArray_unsignedint");
     export_ManagedArray<vec3<float>>(module, "ManagedArrayVec3_float");
     export_ManagedArray<std::complex<float>>(module, "ManagedArray_complexfloat");
