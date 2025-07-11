694d9542b2e2316f24a0fea68c817186c38007b8
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2025-07-11 23:10:48 +0200

    Update TBB version

--- .github/workflows/cibuildwheel-before-build.ps1
+++ .github/workflows/cibuildwheel-before-build.ps1
@@ -3,7 +3,7 @@ $PACKAGE_DIR = $args[0]
 # Install a modern version of CMake for compatibility with modern Visual Studio
 pip install cmake
 
-$TBB_VERSION = "2021.2.0"
+$TBB_VERSION = "2022.1.0"
 $TBB_ZIP = "v${TBB_VERSION}.zip"
 Invoke-WebRequest -Uri "https://github.com/oneapi-src/oneTBB/archive/refs/tags/${TBB_ZIP}" -OutFile "${TBB_ZIP}"
 Expand-Archive -Path "${TBB_ZIP}" -DestinationPath .
