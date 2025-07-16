d62c0dc4751c8bdca3a8df66d249d0da432fb849
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2025-07-16 16:45:20 +0200

    Delete .github/workflows/cibuildwheel-before-test.ps1

--- .github/workflows/cibuildwheel-before-test.ps1
+++
@@ -1,7 +0,0 @@
-$PACKAGE_DIR = $args[0]
-$PYTHON_VERSION = $(python --version)
-
-pip install -U -r ${PACKAGE_DIR}/requirements/requirements-test.txt --progress-bar=off
-
-# Allow parallel tests to speed up CI
-#pip install -U pytest-xdist --progress-bar=off
