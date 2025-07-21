44bc2b42ac0ff92e3b9532943b4207ff64e44a38
Joshua A. Anderson <joaander@umich.edu>
2025-07-21 07:49:03 -0400

    Pin delvwheel to make CI behavior more reproducible.
    Without the pin, a new release of delvewheel might break freud's builds with no action taken on the part of freud's developers.

--- .github/workflows/cibuildwheel-repair-wheel.ps1
+++ .github/workflows/cibuildwheel-repair-wheel.ps1
@@ -2,7 +2,7 @@ $DEST_DIR = $args[0]
 $WHEEL = $args[1]
 
 # Install delvewheel for Windows wheel repairs
-pip install delvewheel
+pip install delvewheel==1.11.0
 
 # From https://cibuildwheel.pypa.io/en/stable/options/#repair-wheel-command
 delvewheel repair --add-path "C:/Program Files (x86)/TBB/bin/" -w "${DEST_DIR}" "${WHEEL}"
