2e5bc707283491f996b8b0b7867958b9fd6c31be
Joshua A. Anderson <joaander@umich.edu>
2025-09-11 10:48:10 -0400

    Remove unused files

--- .github/workflows/environments/update-uv-lockfiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Execute this script to update all lock files to the latest versions of dependencies.
-
-rm requirements*.txt
-
-uv pip compile --python-version 3.13 --python-platform linux requirements-build-sdist.in > requirements-build-sdist.txt
