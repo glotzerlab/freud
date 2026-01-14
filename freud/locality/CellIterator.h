1fd3073cdd8765268891e7db468d672a15c36f56
janbridley <bradleyjenj@gmail.com>
2026-01-14 18:15:16 -0500

    pragma once

--- freud/locality/CellIterator.h
+++ freud/locality/CellIterator.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2010-2025 The Regents of the University of Michigan
 // This file is from the freud project, released under the BSD 3-Clause License.
-
+#pragma once
 #include "LinearCell.h"
 #include "NeighborQuery.h"
 // Iterator structure
@@ -47,7 +47,5 @@ public:
 
     //! Get the next element.
     NeighborBond next() override;
-
-private:
 };
 } // namespace freud::locality
