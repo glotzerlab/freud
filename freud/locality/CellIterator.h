a1582ebdcf1d7d21129b50ff34dd50e11811600c
janbridley <bradleyjenj@gmail.com>
2026-01-14 19:54:55 -0500

    Return bond

--- freud/locality/CellIterator.h
+++ freud/locality/CellIterator.h
@@ -38,15 +38,17 @@ public:
                           unsigned int query_point_idx, float r_max, float r_min, bool exclude_ii)
         : CellIterator(neighbor_query, query_point, query_point_idx, r_max, r_min, exclude_ii)
     {
+        float m_r_max_squared = r_max * r_max;
+        float m_r_min_squared = r_min * r_min;
         vec3<int> xyz = m_cell_query->cell_idx_xyz(query_point);
         unsigned int idx = m_cell_query->get_cell_idx(query_point);
         // TODO: this is only the current cell!
         m_cell_start_index = m_cell_query->m_cell_starts[idx];
         m_cell_end_offset = m_cell_query->m_counts[idx];
         m_cell_data = m_cell_query->m_linear_buffer.data() + m_cell_start_index;
-    //     std::cout << "start index: " << m_cell_start_index << "\n";
-    //     std::cout << "end offset:  " << m_cell_end_offset << "\n";
-    //     std::cout << "m_cell_data:  " << m_cell_data << "\n";
+        //     std::cout << "start index: " << m_cell_start_index << "\n";
+        //     std::cout << "end offset:  " << m_cell_end_offset << "\n";
+        //     std::cout << "m_cell_data:  " << m_cell_data << "\n";
     }
 
     //! Empty Destructor
@@ -56,6 +58,8 @@ public:
     NeighborBond next() override final;
 
 protected:
+    float m_r_max_sq;
+    float m_r_min_sq;
     unsigned int m_query_point_idx;
     unsigned int m_idx = 0;
     unsigned int m_cell_start_index;
@@ -69,15 +73,15 @@ inline NeighborBond CellQueryBallIterator::next()
         m_finished = true;
         return ITERATOR_TERMINATOR;
     }
+    // TODO: handle exclude_ii
     TaggedPosition possible_neighbor = m_cell_data[m_idx++];
-    float r_squared = dot(m_query_point, possible_neighbor.p);
-    // std::cout << "regular loop\n";
-
-    // NeighborBond nb(m_query_point_idx, possible_neighbor.particle_index);
-    // return m_cell_data[m_idx++].p;
-    // m_cell_query->get_cell_idx_safe();
+    const vec3<float> r_ij = possible_neighbor.p - m_query_point;
+    float r_sq = dot(r_ij, r_ij);
+    if (r_sq < m_r_max_sq && r_sq >= m_r_min_sq)
+    {
+        return NeighborBond(m_query_point_idx, possible_neighbor.particle_index, std::sqrt(r_sq), 1, r_ij);
+    }
     // TODO: Implement neighbor search
-    // return nb;
 }
 
 } // namespace freud::locality
