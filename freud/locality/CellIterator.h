7187491f8c43723f8d3732b8a30f1b95fccb5665
janbridley <bradleyjenj@gmail.com>
2026-01-14 19:55:49 -0500

    lint

--- freud/locality/CellIterator.h
+++ freud/locality/CellIterator.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2010-2025 The Regents of the University of Michigan
 // This file is from the freud project, released under the BSD 3-Clause License.
+
 #pragma once
 #include "LinearCell.h"
 #include "NeighborBond.h"
