66fec81a90c09d5e1db80563c02aec8c09ef7c76
Domagoj Fijan <domagoj.fijan@gmail.com>
2026-03-05 16:59:14 -0500

    appease prek import sorting

--- freud/msd.py
+++ freud/msd.py
@@ -1,12 +1,12 @@
 # Copyright (c) 2010-2026 The Regents of the University of Michigan
 # This file is from the freud project, released under the BSD 3-Clause License.
 
-from __future__ import annotations
-
 r"""
 The :class:`freud.msd` module provides functions for computing the
 mean-squared-displacement (MSD) of particles in periodic systems.
 """
+
+from __future__ import annotations
 
 import logging
 from importlib.util import find_spec
